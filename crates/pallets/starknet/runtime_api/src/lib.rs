75293d6979675af5c0bdf0ad3050d3c311bac071
0xTrinityy <trinity0x@protonmail.com>
2024-03-29 14:06:47 +0100

    sync

--- crates/pallets/starknet/runtime_api/src/lib.rs
+++ crates/pallets/starknet/runtime_api/src/lib.rs
@@ -89,6 +89,7 @@ sp_api::decl_runtime_apis! {
         /// If any of the transactions (from both arguments) fails, an error is returned.
         fn re_execute_transactions(transactions: Vec<Transaction>) -> Result<Result<Vec<(TransactionExecutionInfo, CommitmentStateDiff)>, PlaceHolderErrorTypeForFailedStarknetExecution>, DispatchError>;
 
+        fn get_events_for_tx_by_hash(tx_hash: TransactionHash) -> Vec<StarknetEvent>;
         fn get_index_and_tx_for_tx_hash(xts: Vec<<Block as BlockT>::Extrinsic>, chain_id: Felt252Wrapper, tx_hash: TransactionHash) -> Option<(u32, Transaction)>;
 
         /// Return the outcome of the tx execution
