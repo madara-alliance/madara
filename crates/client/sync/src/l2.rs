1555f353dfcd949459c6dd4ac098adff2d4588f2
antiyro <antiyro@gmail.com>
2024-04-02 17:29:28 +0200

    Added log error to show wrong state computation

--- crates/client/sync/src/l2.rs
+++ crates/client/sync/src/l2.rs
@@ -221,6 +221,15 @@ where
 
                         if verify {
                             let (_, block_conv) = rayon::join(ver_l2, || convert_block(block));
+                            let last_l2_state_update =
+                                STARKNET_STATE_UPDATE.read().expect("Failed to acquire read lock on STARKNET_STATE_UPDATE");
+                            if (block_conv.header().global_state_root) != last_l2_state_update.global_root {
+                                log::info!(
+                                    "❗ Computed state root: {:?} doesn't match fetched state: {:?}",
+                                    last_l2_state_update.global_root,
+                                    block_conv.header().global_state_root
+                                );
+                            }
                             block_conv
                         } else {
                             convert_block(block)
