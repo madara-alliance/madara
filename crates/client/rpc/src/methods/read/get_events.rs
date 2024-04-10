b201695b32cafa25896589fa3383a192a041c8f8
0xTrinityy <trinity0x@protonmail.com>
2024-04-10 15:43:41 +0200

    feat: :art: Small modifs to resolves some conflicts

--- crates/client/rpc/src/methods/read/get_events.rs
+++ crates/client/rpc/src/methods/read/get_events.rs
@@ -137,7 +137,7 @@ where
 ///
 /// * `(block_events: Vec<EventWrapper>, continuation_token: usize)` - A tuple of the filtered
 ///   events and the first index which still hasn't been processed block_id and an instance of Block
-fn filter_events_by_params<'a, 'b: 'a>(
+fn filter_events_by_params<'a>(
     events: Vec<EmittedEvent>,
     address: Option<Felt252Wrapper>,
     keys: &'a [Vec<FieldElement>],
@@ -147,7 +147,7 @@ fn filter_events_by_params<'a, 'b: 'a>(
     // Iterate on block events.
     for event in events {
         let match_from_address = address.map_or(true, |addr| addr.0 == event.from_address);
-        // Based on https://github.com/starkware-libs/papyrus
+        // Based on https://github.com/starkware-libs/papyrus/blob/f81f5ec712b4be75ec7da2fc9f3e46c843cc2327/crates/papyrus_rpc/src/v0_5/api/api_impl.rs#L1495-L1499
         let match_keys = keys
             .iter()
             .enumerate()
