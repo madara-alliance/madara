7e34b5f8532938870f7b63cd93183a70c25a1148
cchudant <charlesluc.chudant@orange.fr>
2024-06-21 11:11:25 +0000

    fix: pending block

--- crates/node/src/service/sync.rs
+++ crates/node/src/service/sync.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use std::time::Duration;
 
 use anyhow::Context;
 use dc_db::{DatabaseService, DeoxysBackend};
@@ -26,6 +27,7 @@ pub struct SyncService {
     chain_id: Felt,
     start_params: Option<TelemetryHandle>,
     disabled: bool,
+    pending_block_poll_interval: Duration,
 }
 
 impl SyncService {
@@ -53,6 +55,7 @@ impl SyncService {
             chain_id: config.network.chain_id(),
             start_params: Some(telemetry),
             disabled: config.sync_disabled,
+            pending_block_poll_interval: Duration::from_secs(config.pending_block_poll_interval),
         })
     }
     pub async fn start(&mut self, join_set: &mut JoinSet<anyhow::Result<()>>) -> anyhow::Result<()> {
@@ -67,6 +70,7 @@ impl SyncService {
             starting_block,
             block_metrics,
             chain_id,
+            pending_block_poll_interval,
             ..
         } = self.clone();
         let telemetry = self.start_params.take().context("service already started")?;
@@ -83,6 +87,7 @@ impl SyncService {
                 block_metrics,
                 chain_id,
                 telemetry,
+                pending_block_poll_interval,
             )
             .await
         });
