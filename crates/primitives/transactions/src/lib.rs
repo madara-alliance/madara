a15522891cd57bf73f6ce461dca03974d6c68c74
jbcaron <jbcaron.dev@gmail.com>
2024-06-22 00:28:56 +0400

    add new type `Transaction`

--- crates/primitives/transactions/src/lib.rs
+++ crates/primitives/transactions/src/lib.rs
@@ -1,12 +1,11 @@
 pub mod compute_hash;
 pub mod from_broadcasted_transactions;
+mod from_starknet_provider;
 pub mod getters;
 pub mod to_starknet_core_transaction;
 pub mod utils;
 
 use blockifier::transaction::account_transaction::AccountTransaction;
-use blockifier::transaction::transaction_execution::Transaction;
-use blockifier::transaction::transaction_types::TransactionType;
 
 use starknet_types_core::felt::Felt;
 
@@ -37,22 +36,22 @@ pub enum TxType {
     L1Handler,
 }
 
-impl From<TxType> for TransactionType {
+impl From<TxType> for blockifier::transaction::transaction_types::TransactionType {
     fn from(value: TxType) -> Self {
         match value {
-            TxType::Invoke => TransactionType::InvokeFunction,
-            TxType::Declare => TransactionType::Declare,
-            TxType::DeployAccount => TransactionType::DeployAccount,
-            TxType::L1Handler => TransactionType::L1Handler,
+            TxType::Invoke => Self::InvokeFunction,
+            TxType::Declare => Self::Declare,
+            TxType::DeployAccount => Self::DeployAccount,
+            TxType::L1Handler => Self::L1Handler,
         }
     }
 }
 
-impl From<&Transaction> for TxType {
-    fn from(value: &Transaction) -> Self {
+impl From<&blockifier::transaction::transaction_execution::Transaction> for TxType {
+    fn from(value: &blockifier::transaction::transaction_execution::Transaction) -> Self {
         match value {
-            Transaction::AccountTransaction(tx) => tx.into(),
-            Transaction::L1HandlerTransaction(_) => TxType::L1Handler,
+            blockifier::transaction::transaction_execution::Transaction::AccountTransaction(tx) => tx.into(),
+            blockifier::transaction::transaction_execution::Transaction::L1HandlerTransaction(_) => TxType::L1Handler,
         }
     }
 }
@@ -65,4 +64,181 @@ impl From<&AccountTransaction> for TxType {
             AccountTransaction::Invoke(_) => TxType::Invoke,
         }
     }
+}
+
+/////////////////////////// New transaction types ///////////////////////////
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum Transaction {
+    Invoke(InvokeTransaction),
+    L1Handler(L1HandlerTransaction),
+    Declare(DeclareTransaction),
+    Deploy(DeployTransaction),
+    DeployAccount(DeployAccountTransaction),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum InvokeTransaction {
+    V0(InvokeTransactionV0),
+    V1(InvokeTransactionV1),
+    V3(InvokeTransactionV3),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct InvokeTransactionV0 {
+    pub transaction_hash: Felt,
+    pub max_fee: Felt,
+    pub signature: Vec<Felt>,
+    pub contract_address: Felt,
+    pub entry_point_selector: Felt,
+    pub calldata: Vec<Felt>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct InvokeTransactionV1 {
+    pub transaction_hash: Felt,
+    pub sender_address: Felt,
+    pub calldata: Vec<Felt>,
+    pub max_fee: Felt,
+    pub signature: Vec<Felt>,
+    pub nonce: Felt,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct InvokeTransactionV3 {
+    pub transaction_hash: Felt,
+    pub sender_address: Felt,
+    pub calldata: Vec<Felt>,
+    pub signature: Vec<Felt>,
+    pub nonce: Felt,
+    pub resource_bounds: ResourceBoundsMapping,
+    pub tip: u64,
+    pub paymaster_data: Vec<Felt>,
+    pub account_deployment_data: Vec<Felt>,
+    pub nonce_data_availability_mode: DataAvailabilityMode,
+    pub fee_data_availability_mode: DataAvailabilityMode,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct L1HandlerTransaction {
+    pub transaction_hash: Felt,
+    pub version: Felt,
+    pub nonce: u64,
+    pub contract_address: Felt,
+    pub entry_point_selector: Felt,
+    pub calldata: Vec<Felt>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum DeclareTransaction {
+    V0(DeclareTransactionV0),
+    V1(DeclareTransactionV1),
+    V2(DeclareTransactionV2),
+    V3(DeclareTransactionV3),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct DeclareTransactionV0 {
+    pub transaction_hash: Felt,
+    pub sender_address: Felt,
+    pub max_fee: Felt,
+    pub signature: Vec<Felt>,
+    pub class_hash: Felt,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct DeclareTransactionV1 {
+    pub transaction_hash: Felt,
+    pub sender_address: Felt,
+    pub max_fee: Felt,
+    pub signature: Vec<Felt>,
+    pub nonce: Felt,
+    pub class_hash: Felt,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct DeclareTransactionV2 {
+    pub transaction_hash: Felt,
+    pub sender_address: Felt,
+    pub compiled_class_hash: Felt,
+    pub max_fee: Felt,
+    pub signature: Vec<Felt>,
+    pub nonce: Felt,
+    pub class_hash: Felt,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct DeclareTransactionV3 {
+    pub transaction_hash: Felt,
+    pub sender_address: Felt,
+    pub compiled_class_hash: Felt,
+    pub signature: Vec<Felt>,
+    pub nonce: Felt,
+    pub class_hash: Felt,
+    pub resource_bounds: ResourceBoundsMapping,
+    pub tip: u64,
+    pub paymaster_data: Vec<Felt>,
+    pub account_deployment_data: Vec<Felt>,
+    pub nonce_data_availability_mode: DataAvailabilityMode,
+    pub fee_data_availability_mode: DataAvailabilityMode,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct DeployTransaction {
+    pub transaction_hash: Felt,
+    pub version: Felt,
+    pub contract_address_salt: Felt,
+    pub constructor_calldata: Vec<Felt>,
+    pub class_hash: Felt,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum DeployAccountTransaction {
+    V1(DeployAccountTransactionV1),
+    V3(DeployAccountTransactionV3),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct DeployAccountTransactionV1 {
+    pub transaction_hash: Felt,
+    pub max_fee: Felt,
+    pub signature: Vec<Felt>,
+    pub nonce: Felt,
+    pub contract_address_salt: Felt,
+    pub constructor_calldata: Vec<Felt>,
+    pub class_hash: Felt,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct DeployAccountTransactionV3 {
+    pub transaction_hash: Felt,
+    pub signature: Vec<Felt>,
+    pub nonce: Felt,
+    pub contract_address_salt: Felt,
+    pub constructor_calldata: Vec<Felt>,
+    pub class_hash: Felt,
+    pub resource_bounds: ResourceBoundsMapping,
+    pub tip: u64,
+    pub paymaster_data: Vec<Felt>,
+    pub nonce_data_availability_mode: DataAvailabilityMode,
+    pub fee_data_availability_mode: DataAvailabilityMode,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum DataAvailabilityMode {
+    L1,
+    L2,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+
+pub struct ResourceBoundsMapping {
+    pub l1_gas: ResourceBounds,
+    pub l2_gas: ResourceBounds,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct ResourceBounds {
+    pub max_amount: u64,
+    pub max_price_per_unit: u128,
 }
