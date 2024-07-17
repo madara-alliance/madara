23b01598e1733cc11019b04da19c83e5be8cb8c2
mohiiit <mohit18343@iiitd.ac.in>
2024-07-17 16:57:56 +0530

    setup for eth done

---
+++ crates/client/sync/src/eth/src/config.rs
@@ -0,0 +1,161 @@
+//! Base Ethereum client configuration.
+//!
+//! Use it as is or reuse top-level fields in your config ("inherit")
+//! in order to share the same configuration file.
+//!
+//! struct MyServiceConfig {
+//!     pub provider: EthereumProviderConfig,
+//!     pub wallet: Option<EthereumWalletConfig>,
+//!     pub contracts: Option<StarknetContracts>,
+//! }
+//!
+//! Default provider and wallet configurations are set for use with Anvil
+//! - local Ethereum environment.
+
+use std::fs::File;
+use std::path::PathBuf;
+
+use alloy::primitives::{
+    address, b128, b256, b512, b64, bytes, fixed_bytes, Address, Bytes, FixedBytes,
+};
+use ethers::types::{ H160};
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+// use crate::oracle::OracleConfig;
+
+/// Default Anvil local endpoint
+pub const DEFAULT_RPC_ENDPOINT: &str = "http://127.0.0.1:8545";
+/// Default Anvil chain ID
+pub const DEFAULT_CHAIN_ID: u64 = 31337;
+/// Default private key derived from starting Anvil as follows:
+/// anvil -b 5 --config-out $BUILD_DIR/anvil.json
+/// PRE_PRIVATE=$(jq -r '.private_keys[0]' $BUILD_DIR/anvil.json)
+pub const DEFAULT_PRIVATE_KEY: &str = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct EthereumClientConfig {
+    #[serde(default)]
+    pub provider: EthereumProviderConfig,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub wallet: Option<EthereumWalletConfig>,
+    #[serde(default)]
+    pub contracts: StarknetContracts
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum EthereumProviderConfig {
+    Http(HttpProviderConfig),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum EthereumWalletConfig {
+    Local(LocalWalletConfig),
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct StarknetContracts {
+    pub core_contract: String,
+    // #[serde(default, skip_serializing_if = "Option::is_none")]
+    // pub verifier_contract: Option<String>,
+    // #[serde(default, skip_serializing_if = "Option::is_none")]
+    // pub memory_pages_contract: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct HttpProviderConfig {
+    #[serde(default = "default_rpc_endpoint")]
+    pub rpc_endpoint: String,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub tx_poll_interval_ms: Option<u64>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub gas_price_poll_ms: Option<u64>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LocalWalletConfig {
+    #[serde(default = "default_chain_id")]
+    pub chain_id: u64,
+    #[serde(default = "default_private_key")]
+    pub private_key: String,
+}
+
+fn default_rpc_endpoint() -> String {
+    DEFAULT_RPC_ENDPOINT.into()
+}
+
+fn default_chain_id() -> u64 {
+    DEFAULT_CHAIN_ID
+}
+
+fn default_private_key() -> String {
+    DEFAULT_PRIVATE_KEY.to_string()
+}
+
+impl Default for HttpProviderConfig {
+    fn default() -> Self {
+        Self { rpc_endpoint: default_rpc_endpoint(), tx_poll_interval_ms: None, gas_price_poll_ms: None }
+    }
+}
+
+impl Default for EthereumProviderConfig {
+    fn default() -> Self {
+        Self::Http(HttpProviderConfig::default())
+    }
+}
+
+impl EthereumProviderConfig {
+    pub fn rpc_endpoint(&self) -> &String {
+        match self {
+            Self::Http(config) => &config.rpc_endpoint,
+        }
+    }
+
+    pub fn gas_price_poll_ms(&self) -> Option<u64> {
+        match self {
+            Self::Http(config) => config.gas_price_poll_ms,
+        }
+    }
+}
+
+impl Default for LocalWalletConfig {
+    fn default() -> Self {
+        Self { chain_id: default_chain_id(), private_key: default_private_key() }
+    }
+}
+
+impl Default for EthereumWalletConfig {
+    fn default() -> Self {
+        Self::Local(LocalWalletConfig::default())
+    }
+}
+
+impl StarknetContracts {
+    pub fn core_contract(&self) -> Result<Address, Error> {
+        Address::parse_checksummed(&self.core_contract, None)
+            .map_err(|e| Error::AddressParseError(e))
+    }
+
+    // pub fn verifier_contract(&self) -> Result<Address, Error> {
+    //     self.verifier_contract
+    //         .as_ref()
+    //         .map(|s| parse_contract_address(s))
+    //         .ok_or(Error::ContractAddressUndefined("verifier"))?
+    // }
+    //
+    // pub fn memory_pages_contract(&self) -> Result<Address, Error> {
+    //     self.memory_pages_contract
+    //         .as_ref()
+    //         .map(|s| parse_contract_address(s))
+    //         .ok_or(Error::ContractAddressUndefined("memory pages"))?
+    // }
+}
+
+impl EthereumClientConfig {
+    pub fn from_json_file(path: &PathBuf) -> Result<Self, Error> {
+        let file = File::open(path).map_err(Error::ConfigReadFromFile)?;
+        serde_json::from_reader(file).map_err(Error::ConfigDecodeFromJson)
+    }
+}
