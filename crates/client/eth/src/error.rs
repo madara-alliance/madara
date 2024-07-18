fa702bfc53b9c5b664ad51b4554a1757788f300b
mohiiit <mohit18343@iiitd.ac.in>
2024-07-19 04:51:44 +0530

    updated the locatoin of eth crate

--- crates/client/eth/src/error.rs
+++ crates/client/eth/src/error.rs
@@ -1,12 +1,12 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("Failed to parse provider URL: {0}")]
-    ProviderUrlParse(#[source] url::ParseError),
+    ProviderUrlParse(#[from] url::ParseError),
     #[error("Failed to parse contract address: {0}")]
-    AddressParseError(#[source] alloy::primitives::AddressError),
+    AddressParseError(#[from] alloy::primitives::AddressError),
     #[error("Failed to read config from file: {0}")]
-    ConfigReadFromFile(#[source] std::io::Error),
+    ConfigReadFromFile(#[from] std::io::Error),
     #[error("Failed to decode from JSON: {0}")]
-    ConfigDecodeFromJson(#[source] serde_json::Error),
+    ConfigDecodeFromJson(#[from] serde_json::Error),
 
 }
