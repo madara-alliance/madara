95b4b50cfb422fa95b572716f0e81f71b0e0ee58
akhercha <akherchache@pm.me>
2024-08-29 10:42:08 +0200

    dev(block_import_refactoring): Removed TODO

--- crates/primitives/validation/src/lib.rs
+++ crates/primitives/validation/src/lib.rs
@@ -6,7 +6,6 @@ pub trait Validate: Send + Sync + Sized + 'static {
     type Context: Send + 'static;
     type ValidationError: Send;
 
-    // TODO: context should be optional
     fn validate(self, context: &Self::Context) -> Result<Self::Output, Self::ValidationError>;
 
     async fn spawn_validate(
