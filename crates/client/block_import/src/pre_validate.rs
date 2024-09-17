a9d3c65aeaf124c211344560587d0b1b2a81b364
antiyro <antiyro@gmail.com>
2024-09-17 12:42:12 +0100

    cleaned pre validate

--- crates/client/block_import/src/pre_validate.rs
+++ crates/client/block_import/src/pre_validate.rs
@@ -172,15 +172,15 @@ fn class_conversion(
         }
         DeclaredClass::Legacy(legacy) => {
             log::trace!("Converting legacy class with hash {:#x}", legacy.class_hash);
-            // TODO: verify that the class hash is correct
             if !validation.trust_class_hashes {
                 let class_hash = legacy
-                    .contract_class
-                    .compute_class_hash()
-                    .map_err(|e| BlockImportError::ComputeClassHash { class_hash: legacy.class_hash, error: e })?;
-                log::info!("Computed class hash {:#x}", class_hash);
+                .contract_class
+                .compute_class_hash()
+                .map_err(|e| BlockImportError::ComputeClassHash { class_hash: legacy.class_hash, error: e })?;
                 if class_hash != legacy.class_hash {
-                    return Err(BlockImportError::ClassHash { got: legacy.class_hash, expected: class_hash });
+                    // TODO: For now we skip the exceptions for the legacy class hash mismatch
+                    log::debug!("Class hash mismatch: got {:#x}, expected {:#x}", class_hash, legacy.class_hash, );
+                    // return Err(BlockImportError::ClassHash { got: class_hash, expected: legacy.class_hash });
                 }
             }
             Ok(ConvertedClass::Legacy(LegacyConvertedClass {
