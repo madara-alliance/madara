1e07472d3643054ecffda7f086dd15127a974c6b
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2024-09-25 13:33:02 +0530

    remove ignore

--- crates/tests/src/rpc/read.rs
+++ crates/tests/src/rpc/read.rs
@@ -74,7 +74,6 @@ mod test_rpc_read_calls {
     /// ```
     #[rstest]
     #[tokio::test]
-    #[ignore]
     async fn test_block_hash_and_number_works(_set_workdir: ()) {
         let madara = get_shared_state().await;
         let json_client = JsonRpcClient::new(HttpTransport::new(madara.rpc_url.clone()));
@@ -109,7 +108,6 @@ mod test_rpc_read_calls {
     /// ```
     #[rstest]
     #[tokio::test]
-    #[ignore]
     async fn test_get_block_txn_count_works(_set_workdir: ()) {
         let madara = get_shared_state().await;
         let json_client = JsonRpcClient::new(HttpTransport::new(madara.rpc_url.clone()));
