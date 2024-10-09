5758897974e2a9e885d341e605d9164769790758
Shams Asari <shams@hibzahim.com>
2024-10-09 16:19:04 +0100

    bug(tests): Fix local test scripts (#316)
    The test scripts in scripts didn't work due to missing environment variables needed by the tests.
    
    The tests also relied on the working directory being set such that test artifacts in source could be read. This has been fixed with include_bytes! and include_str! isntead, and the the working directory for the test is no longer modified.

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -11,10 +11,15 @@ anvil --fork-url https://eth.merkle.io --fork-block-number 20395662 &
 subshell() {
     set -e
     cargo build --bin madara --profile dev
+
+    export COVERAGE_BIN=$(realpath target/debug/madara)
+    export ETH_FORK_URL=https://eth.merkle.io
+
     # wait for anvil
     while ! nc -z localhost 8545; do
         sleep 1
     done
+
     cargo test --profile dev $@
 }
 
