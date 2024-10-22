007de7817bcc9a146d14b40cdde1b18f8a1b34aa
jbcaron <jbcaron.dev@gmail.com>
2024-10-22 22:05:20 +0400

    replacing the reqwest feeder client with hyper

--- crates/client/gateway/src/client/builder.rs
+++ crates/client/gateway/src/client/builder.rs
@@ -1,12 +1,13 @@
-use reqwest::{
-    header::{HeaderMap, HeaderName, HeaderValue},
-    Client,
-};
+use hyper::header::{HeaderMap, HeaderName, HeaderValue};
 use url::Url;
+
+use hyper::client::HttpConnector;
+use hyper::{Body, Client};
+use hyper_tls::HttpsConnector;
 
 #[derive(Debug, Clone)]
 pub struct FeederClient {
-    pub(crate) client: Client,
+    pub(crate) client: Client<HttpsConnector<HttpConnector>>,
     #[allow(dead_code)]
     pub(crate) gateway_url: Url,
     pub(crate) feeder_gateway_url: Url,
@@ -15,12 +16,16 @@ pub struct FeederClient {
 
 impl FeederClient {
     pub fn new(gateway_url: Url, feeder_gateway_url: Url) -> Self {
-        Self { client: Client::new(), gateway_url, feeder_gateway_url, headers: HeaderMap::new() }
+        let connector = HttpsConnector::new();
+        let client = Client::builder().build::<_, Body>(connector);
+        Self { client, gateway_url, feeder_gateway_url, headers: HeaderMap::new() }
     }
 
     pub fn new_with_headers(gateway_url: Url, feeder_gateway_url: Url, headers: &[(HeaderName, HeaderValue)]) -> Self {
+        let connector = HttpsConnector::new();
+        let client = Client::builder().build::<_, Body>(connector);
         let headers = headers.iter().cloned().collect();
-        Self { client: Client::new(), gateway_url, feeder_gateway_url, headers }
+        Self { client, gateway_url, feeder_gateway_url, headers }
     }
 
     pub fn add_header(&mut self, name: HeaderName, value: HeaderValue) {
