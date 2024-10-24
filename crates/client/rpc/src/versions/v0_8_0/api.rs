3ec31788a834474f2e31d3fbe3031f5d0f1f062c
trantorian <114066155+Trantorian1@users.noreply.github.com>
2024-10-24 11:15:11 +0200

    fix(foo): removed dummy function

--- crates/client/rpc/src/versions/v0_8_0/api.rs
+++ crates/client/rpc/src/versions/v0_8_0/api.rs
@@ -4,9 +4,6 @@ use crate::errors::WsResult;
 
 #[versioned_rpc("V0_8_0", "starknet")]
 pub trait StarknetWsRpcApi {
-    #[subscription(name = "subscribe_foo", unsubscribe = "unsubscribe_foo", item = String, param_kind = map)]
-    async fn foo(&self, block_id: starknet_core::types::BlockId) -> jsonrpsee::core::SubscriptionResult;
-
     #[subscription(name = "subscribeNewHeads", unsubscribe = "unsubscribe", item = starknet_api::block::BlockHeader, param_kind = map)]
     async fn subscribe_new_heads(&self, block_id: starknet_core::types::BlockId) -> WsResult;
 }
--- crates/client/rpc/src/versions/v0_8_0/methods/ws/lib.rs
+++ crates/client/rpc/src/versions/v0_8_0/methods/ws/lib.rs
@@ -9,33 +9,6 @@ use super::BLOCK_PAST_LIMIT;
 
 #[jsonrpsee::core::async_trait]
 impl StarknetWsRpcApiV0_8_0Server for crate::Starknet {
-    async fn foo(
-        &self,
-        pending: jsonrpsee::PendingSubscriptionSink,
-        block_id: starknet_core::types::BlockId,
-    ) -> jsonrpsee::core::SubscriptionResult {
-        let sink = pending.accept().await?;
-
-        let msg = jsonrpsee::SubscriptionMessage::from(format!("{block_id:?}"));
-        sink.send(msg).await?;
-
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-
-        #[derive(serde::Serialize)]
-        struct StarknetSubscriptionError<'a> {
-            code: u32,
-            message: &'a str,
-        }
-
-        let err = serde_json::to_string(&StarknetSubscriptionError {
-            code: 68,
-            message: "Cannot go back more than 1024 blocks",
-        })
-        .unwrap();
-
-        return Err(err.into());
-    }
-
     async fn subscribe_new_heads(
         &self,
         pending: jsonrpsee::PendingSubscriptionSink,
