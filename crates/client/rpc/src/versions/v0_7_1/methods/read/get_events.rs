b84ff3368c5f03ecb1af707c970ae47df530f008
trantorian <114066155+Trantorian1@users.noreply.github.com>
2024-11-05 16:32:42 +0100

    perf(getEvents): refactored `get_block_events`

--- crates/client/rpc/src/versions/v0_7_1/methods/read/get_events.rs
+++ crates/client/rpc/src/versions/v0_7_1/methods/read/get_events.rs
@@ -38,46 +38,46 @@ pub async fn get_events(starknet: &Starknet, filter: EventFilterWithPage) -> Sta
         return Err(StarknetRpcApiError::PageSizeTooBig);
     }
 
-    // TODO: this function doesn't do much, should be hoisted out
-    // Get the block numbers for the requested range
-    let (from_block, to_block, latest_block) =
-        block_range(starknet, filter.event_filter.from_block, filter.event_filter.to_block)?;
+    let latest_block = starknet.get_block_n(&BlockId::Tag(BlockTag::Latest))?;
+    let from_block = match filter.event_filter.from_block {
+        Some(BlockId::Tag(BlockTag::Pending)) => latest_block + 1,
+        Some(block_id) => starknet.get_block_n(&block_id)?,
+        None => 0,
+    };
+    let to_block = match filter.event_filter.to_block {
+        Some(BlockId::Tag(BlockTag::Pending)) => latest_block + 1,
+        Some(block_id) => starknet.get_block_n(&block_id)?,
+        None => latest_block,
+    };
+
+    if from_block > to_block {
+        return Ok(EventsPage { events: vec![], continuation_token: None });
+    }
 
     let continuation_token = match filter.result_page_request.continuation_token {
         Some(token) => ContinuationToken::parse(token).map_err(|_| StarknetRpcApiError::InvalidContinuationToken)?,
         None => ContinuationToken { block_n: from_block, event_n: 0 },
     };
-
-    // PERF: this is minor but this could happen earlier
-    // Verify that the requested range is valid
-    if from_block > to_block {
-        return Ok(EventsPage { events: vec![], continuation_token: None });
-    }
-
     let from_block = continuation_token.block_n;
-    // PERF: this should at least be pre-allocated to some sensible default
-    let mut filtered_events: Vec<EmittedEvent> = Vec::new();
 
     // PERF: we should truncate from_block to the creation block of the contract
     // if it is less than that
-    for current_block in from_block..=to_block {
+    let mut filtered_events: Vec<EmittedEvent> = Vec::with_capacity(16);
+    for block_n in from_block..=to_block {
         // PERF: this check can probably be hoisted out of this loop
-        let (_pending, block) = if current_block <= latest_block {
+        let block = if block_n <= latest_block {
             // PERF: This is probably the main bottleneck: we should be able to
             // mitigate this by implementing a db iterator
-            (false, starknet.get_block(&BlockId::Number(current_block))?)
+            starknet.get_block(&BlockId::Number(block_n))?
         } else {
-            (true, starknet.get_block(&BlockId::Tag(BlockTag::Pending))?)
+            starknet.get_block(&BlockId::Tag(BlockTag::Pending))?
         };
 
         // PERF: collection needs to be more efficient
-        let block_filtered_events: Vec<EmittedEvent> = get_block_events(starknet, &block)
-            .into_iter()
-            .filter(|event| event_match_filter(event, from_address, &keys))
-            .collect();
+        let block_filtered_events: Vec<EmittedEvent> = get_block_events(block, from_address, &keys);
 
         // PERF: this condition needs to be moved out the loop as it needs to happen only once
-        if current_block == from_block && (block_filtered_events.len() as u64) < continuation_token.event_n {
+        if block_n == from_block && (block_filtered_events.len() as u64) < continuation_token.event_n {
             return Err(StarknetRpcApiError::InvalidContinuationToken);
         }
 
@@ -85,7 +85,7 @@ pub async fn get_events(starknet: &Starknet, filter: EventFilterWithPage) -> Sta
         #[allow(clippy::iter_skip_zero)]
         let block_filtered_reduced_events: Vec<EmittedEvent> = block_filtered_events
             .into_iter()
-            .skip(if current_block == from_block { continuation_token.event_n as usize } else { 0 })
+            .skip(if block_n == from_block { continuation_token.event_n as usize } else { 0 })
             .take(chunk_size as usize - filtered_events.len())
             .collect();
 
@@ -97,8 +97,8 @@ pub async fn get_events(starknet: &Starknet, filter: EventFilterWithPage) -> Sta
 
         if filtered_events.len() == chunk_size as usize {
             let event_n =
-                if current_block == from_block { continuation_token.event_n + chunk_size } else { num_events as u64 };
-            let token = Some(ContinuationToken { block_n: current_block, event_n }.to_string());
+                if block_n == from_block { continuation_token.event_n + chunk_size } else { num_events as u64 };
+            let token = Some(ContinuationToken { block_n, event_n }.to_string());
 
             return Ok(EventsPage { events: filtered_events, continuation_token: token });
         }
@@ -106,40 +106,11 @@ pub async fn get_events(starknet: &Starknet, filter: EventFilterWithPage) -> Sta
     Ok(EventsPage { events: filtered_events, continuation_token: None })
 }
 
-#[inline]
-fn event_match_filter(event: &EmittedEvent, address: Option<Felt>, keys: &[Vec<Felt>]) -> bool {
-    let match_from_address = address.map_or(true, |addr| addr == event.from_address);
-    // PERF: HOLY FUCK WE CHECK ALL EVENTS EVEN IF THEY COME FROM THE WRONG
-    // ADDRESS
-    let match_keys = keys
-        .iter()
-        .enumerate()
-        .all(|(i, keys)| event.keys.len() > i && (keys.is_empty() || keys.contains(&event.keys[i])));
-    // PERF: this can be short-circuited
-    match_from_address && match_keys
-}
-
-// TODO: remove this function, this code can be dealt with manually
-fn block_range(
-    starknet: &Starknet,
-    from_block: Option<BlockId>,
-    to_block: Option<BlockId>,
-) -> StarknetRpcResult<(u64, u64, u64)> {
-    let latest_block_n = starknet.get_block_n(&BlockId::Tag(BlockTag::Latest))?;
-    let from_block_n = match from_block {
-        Some(BlockId::Tag(BlockTag::Pending)) => latest_block_n + 1,
-        Some(block_id) => starknet.get_block_n(&block_id)?,
-        None => 0,
-    };
-    let to_block_n = match to_block {
-        Some(BlockId::Tag(BlockTag::Pending)) => latest_block_n + 1,
-        Some(block_id) => starknet.get_block_n(&block_id)?,
-        None => latest_block_n,
-    };
-    Ok((from_block_n, to_block_n, latest_block_n))
-}
-
-fn get_block_events(_starknet: &Starknet, block: &MadaraMaybePendingBlock) -> Vec<EmittedEvent> {
+fn get_block_events(
+    block: MadaraMaybePendingBlock,
+    address: Option<Felt>,
+    keys: &[Vec<Felt>],
+) -> Vec<starknet_core::types::EmittedEvent> {
     // PERF:: this check can probably be removed by handling pending blocks
     // separatly
     let (block_hash, block_number) = match &block.info {
@@ -147,21 +118,41 @@ fn get_block_events(_starknet: &Starknet, block: &MadaraMaybePendingBlock) -> Ve
         MadaraMaybePendingBlockInfo::NotPending(block) => (Some(block.block_hash), Some(block.header.block_number)),
     };
 
-    let tx_hash_and_events = block.inner.receipts.iter().flat_map(|receipt| {
-        let tx_hash = receipt.transaction_hash();
-        receipt.events().iter().map(move |events| (tx_hash, events))
-    });
+    block
+        .inner
+        .receipts
+        .into_iter()
+        .flat_map(move |receipt| {
+            let transaction_hash = receipt.transaction_hash();
 
-    // PERF: clone here is brutal, there must be a way to take ownership of this
-    // data
-    tx_hash_and_events
-        .map(|(transaction_hash, event)| EmittedEvent {
-            from_address: event.from_address,
-            keys: event.keys.clone(),
-            data: event.data.clone(),
-            block_hash,
-            block_number,
-            transaction_hash,
+            receipt.events_owned().into_iter().filter_map(move |event| {
+                if address.is_some() && address.unwrap() != event.from_address {
+                    return None;
+                }
+
+                // Keys are matched as follows:
+                //
+                // - `keys` is an array of Felt
+                // - `keys[n]` represents allowed value for event key at index n
+                // - so `event.keys[n]` needs to match any value in `keys[n]`
+                let match_keys = keys
+                    .iter()
+                    .enumerate()
+                    .all(|(i, keys)| event.keys.len() >= i && (keys.is_empty() || keys.contains(&event.keys[i])));
+
+                if !match_keys {
+                    None
+                } else {
+                    Some(EmittedEvent {
+                        from_address: event.from_address,
+                        keys: event.keys,
+                        data: event.data,
+                        block_hash,
+                        block_number,
+                        transaction_hash,
+                    })
+                }
+            })
         })
         .collect()
 }
