c624e671ec265e1a3ec4a00f2f849d3b5282f120
Stephen Shelton <steve@brewcraft.org>
2025-01-02 08:20:14 -0700

    Update crates/madara/primitives/chain_config/src/chain_config.rs
    Co-authored-by: Trantorian1 <114066155+Trantorian1@users.noreply.github.com>

--- crates/madara/primitives/chain_config/src/chain_config.rs
+++ crates/madara/primitives/chain_config/src/chain_config.rs
@@ -53,7 +53,7 @@ const BLOCKIFIER_VERSIONED_CONSTANTS_JSON_0_13_1: &[u8] =
 const BLOCKIFIER_VERSIONED_CONSTANTS_JSON_0_13_1_1: &[u8] =
     include_bytes!("../resources/versioned_constants_13_1_1.json");
 const BLOCKIFIER_VERSIONED_CONSTANTS_JSON_0_13_2: &[u8] =
-    include_bytes!("./../resources/versioned_constants_13_2.json");
+    include_bytes!("../resources/versioned_constants_13_2.json");
 
 lazy_static::lazy_static! {
     pub static ref BLOCKIFIER_VERSIONED_CONSTANTS_0_13_2: VersionedConstants =
