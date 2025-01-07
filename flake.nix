91ec16c2918f40a7dc458f72d1decd3bd9e53dff
Charpa <jbcaron.dev@gmail.com>
2025-01-07 15:47:04 +0100

    ci: integrate yamllint and alejandra linters

--- flake.nix
+++ flake.nix
@@ -1,21 +1,25 @@
 {
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
 
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      in
-      {
-
+      in {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             openssl
@@ -26,13 +30,15 @@
             yq
           ];
 
-          buildInputs = with pkgs; [
-            rustToolchain
-            clang
-            rocksdb
-          ] ++ lib.optionals stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.Security
-          ];
+          buildInputs = with pkgs;
+            [
+              rustToolchain
+              clang
+              rocksdb
+            ]
+            ++ lib.optionals stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.Security
+            ];
 
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           PROTOC = "${pkgs.protobuf}/bin/protoc";
