ca7a4c4bc11f3c27404fe864cb7ed25f28a20960
Stephen Shelton <steve@brewcraft.org>
2025-01-24 19:01:26 +0000

    Reflect upstream fn name change

--- orchestrator/crates/prover-clients/sharp-service/tests/lib.rs
+++ orchestrator/crates/prover-clients/sharp-service/tests/lib.rs
@@ -44,7 +44,7 @@ async fn prover_client_submit_task_works() {
     );
     let customer_id = get_env_var_or_panic("MADARA_ORCHESTRATOR_SHARP_CUSTOMER_ID");
     let sharp_add_job_call = server.mock(|when, then| {
-        when.path_includes("/add_job").query_param("customer_id", customer_id.as_str());
+        when.path_contains("/add_job").query_param("customer_id", customer_id.as_str());
         then.status(200).body(serde_json::to_vec(&sharp_response).unwrap());
     });
 
@@ -83,7 +83,7 @@ async fn prover_client_get_task_status_works(#[case] cairo_job_status: CairoJobS
     let customer_id = get_env_var_or_panic("MADARA_ORCHESTRATOR_SHARP_CUSTOMER_ID");
 
     let sharp_add_job_call = server.mock(|when, then| {
-        when.path_includes("/get_status").query_param("customer_id", customer_id.as_str());
+        when.path_contains("/get_status").query_param("customer_id", customer_id.as_str());
         then.status(200).body(serde_json::to_vec(&get_task_status_sharp_response(&cairo_job_status)).unwrap());
     });
 
