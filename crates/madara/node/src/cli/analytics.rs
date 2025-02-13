36ee25322034ea45cee8f5156c5d0d8844cae16b
Gustavo Madeira Krieger <gustavo.m.krieger@gmail.com>
2025-02-13 19:09:11 -0300

    feat(node): load args from file

--- crates/madara/node/src/cli/analytics.rs
+++ crates/madara/node/src/cli/analytics.rs
@@ -1,9 +1,10 @@
 use clap::Args;
 use mp_utils::parsers::parse_url;
+use serde::{Deserialize, Serialize};
 use url::Url;
 
 /// Parameters used to config analytics.
-#[derive(Debug, Clone, Args)]
+#[derive(Debug, Clone, Args, Deserialize, Serialize)]
 pub struct AnalyticsParams {
     /// Name of the service.
     #[arg(env = "MADARA_ANALYTICS_SERVICE_NAME", long, alias = "analytics", default_value = "madara_analytics")]
