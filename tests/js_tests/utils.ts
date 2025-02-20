c984ffbb0b836e7441d1cdecf770ed09d7405f4b
Stephen Shelton <steve@brewcraft.org>
2025-02-20 20:20:34 +0000

    Only look two directories up

--- tests/js_tests/utils.ts
+++ tests/js_tests/utils.ts
@@ -8,7 +8,7 @@ export const readContractCasm = (name: string): CompiledSierraCasm =>
       .readFileSync(
         path.resolve(
           __dirname,
-          `../../../cairo/target/dev/${name}.compiled_contract_class.json`,
+          `../../cairo/target/dev/${name}.compiled_contract_class.json`,
         ),
       )
       .toString("ascii"),
@@ -20,7 +20,7 @@ export const readContractSierra = (name: string): CompiledSierra =>
       .readFileSync(
         path.resolve(
           __dirname,
-          `../../../cairo/target/dev/${name}.contract_class.json`,
+          `../../cairo/target/dev/${name}.contract_class.json`,
         ),
       )
       .toString("ascii"),
@@ -32,7 +32,7 @@ export const readContractSierraInArtifacts = (name: string): CompiledSierra =>
       .readFileSync(
         path.resolve(
           __dirname,
-          `../../../cairo-artifacts/${name}.contract_class.json`,
+          `../../cairo-artifacts/${name}.contract_class.json`,
         ),
       )
       .toString("ascii"),
