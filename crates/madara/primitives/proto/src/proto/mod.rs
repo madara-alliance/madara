cb919ad2162c79f0d183682aa4d601ec99f52486
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-02-20 14:15:19 +0100

    feat(malachite): some progress on implementing primitive types

--- crates/madara/primitives/proto/src/proto/mod.rs
+++ crates/madara/primitives/proto/src/proto/mod.rs
@@ -1,4 +1,5 @@
 pub mod classes;
+pub mod consensus;
 pub mod events;
 pub mod headers;
 pub mod model_primitives;
--- crates/madara/primitives/proto/src/proto/proposal.rs
+++ crates/madara/primitives/proto/src/proto/proposal.rs
@@ -13,3 +13,18 @@ impl PartialOrd for model::StreamMessage {
         Some(self.cmp(other))
     }
 }
+
+impl Eq for model::ConsensusSignature {}
+
+// FIXME: Can't really know how to order this until we know the signature schema
+impl Ord for model::ConsensusSignature {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        unimplemented!()
+    }
+}
+
+impl PartialOrd for model::ConsensusSignature {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        unimplemented!()
+    }
+}
