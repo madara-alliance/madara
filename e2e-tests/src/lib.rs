d247dda63c47f427d8a317144935fb6fb4a00ae4
cchudant <cchudant@proton.me>
2025-03-04 16:43:22 +0100

    re-add orchestrator e2e tests

--- e2e-tests/src/lib.rs
+++ e2e-tests/src/lib.rs
@@ -1,26 +1,26 @@
-// pub mod anvil;
-// pub mod mock_server;
-// pub mod mongodb;
-// pub mod node;
-// pub mod sharp;
-// pub mod starknet_client;
-// pub mod utils;
+pub mod anvil;
+pub mod mock_server;
+pub mod mongodb;
+pub mod node;
+pub mod sharp;
+pub mod starknet_client;
+pub mod utils;
 
-// use std::net::TcpListener;
+use std::net::TcpListener;
 
-// pub use mongodb::MongoDbServer;
-// pub use node::Orchestrator;
-// pub use orchestrator::database::mongodb::MongoDb as MongoDbClient;
+pub use mongodb::MongoDbServer;
+pub use node::Orchestrator;
+pub use orchestrator::database::mongodb::MongoDb as MongoDbClient;
 
-// const MIN_PORT: u16 = 49_152;
-// const MAX_PORT: u16 = 65_535;
+const MIN_PORT: u16 = 49_152;
+const MAX_PORT: u16 = 65_535;
 
-// fn get_free_port() -> u16 {
-//     for port in MIN_PORT..=MAX_PORT {
-//         if let Ok(listener) = TcpListener::bind(("127.0.0.1", port)) {
-//             return listener.local_addr().expect("No local addr").port();
-//         }
-//         // otherwise port is occupied
-//     }
-//     panic!("No free ports available");
-// }
+fn get_free_port() -> u16 {
+    for port in MIN_PORT..=MAX_PORT {
+        if let Ok(listener) = TcpListener::bind(("127.0.0.1", port)) {
+            return listener.local_addr().expect("No local addr").port();
+        }
+        // otherwise port is occupied
+    }
+    panic!("No free ports available");
+}
