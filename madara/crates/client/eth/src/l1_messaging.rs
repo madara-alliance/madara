6847c791b43f17eb2d9f8df20bc38179ca0b8e6c
Herman Obst Demaestri <hodemaestri@gmail.com>
2025-03-21 18:14:25 -0300

    add comment for clarity

--- madara/crates/client/eth/src/l1_messaging.rs
+++ madara/crates/client/eth/src/l1_messaging.rs
@@ -435,6 +435,10 @@ mod l1_messaging_tests {
         let current_nonce = mempool.backend.get_l1_messaging_nonce_latest().unwrap().unwrap();
         assert_eq!(current_nonce, expected_nonce);
 
+        // Check that the L1 message correctly trigger an L1 handler tx, which is accepted in Mempool
+        // As it has the first nonce (and no other L1 message was received before) we can take it from Mempool
+        // TODO: we can add a test case on which a message with Nonce = 1 is being received before this one
+        // and check we cant take it until the first on is processed
         let (handler_tx, _handler_tx_hash) = match mempool.tx_take().unwrap().tx {
             Transaction::L1HandlerTransaction(handler_tx) => (handler_tx.tx, handler_tx.tx_hash.0),
             Transaction::AccountTransaction(_) => panic!("Expecting L1 handler transaction"),
