9248b90df057896dd4f36ce3c639fe38aa1c3e22
Tim B <79199034+timbrinded@users.noreply.github.com>
2025-03-21 09:18:14 +0000

    review comments

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,8 +5,18 @@ echo ">> Starting setup.sh script execution."
 # Uncomment this if you want to remove the db on each container start
 # rm -rf /tmp/madara
 
+echo ">> Rust version:"
 rustup show
+
+echo ">> Node version:"
 node --version
+
+asdf install scarb 2.8.2
+asdf set -u scarb 2.8.2 
+echo ">> Scarb version:"
+scarb --version
+
+echo ">> Python version:"
 python --version
 
 # Uncomment this if you want to run madara on container start
