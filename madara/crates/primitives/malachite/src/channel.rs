742891b0732aee625aadf3d57a658ea044987e53
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-03-21 18:34:25 +0100

    feat(mq): debugged message queue atomicity

--- madara/crates/primitives/malachite/src/channel.rs
+++ madara/crates/primitives/malachite/src/channel.rs
@@ -2,39 +2,44 @@
 use loom::alloc;
 #[cfg(test)]
 use loom::sync;
+#[cfg(test)]
+use loom::sync::Notify;
 
 #[cfg(not(test))]
 use std::alloc;
 #[cfg(not(test))]
 use std::sync;
+#[cfg(not(test))]
+use tokio::sync::Notify;
 
-pub struct MqSender<T: Send + Clone> {
+pub struct MqSender<T: Send + Clone + std::fmt::Debug + tracing::Value> {
     queue: sync::Arc<MessageQueue<T>>,
-    wake: sync::Arc<tokio::sync::Notify>,
+    wake: sync::Arc<Notify>,
 }
 
-pub struct MqReceiver<T: Send + Clone> {
+pub struct MqReceiver<T: Send + Clone + std::fmt::Debug + tracing::Value> {
     queue: sync::Arc<MessageQueue<T>>,
-    wake: sync::Arc<tokio::sync::Notify>,
+    wake: sync::Arc<Notify>,
 }
 
 #[must_use]
-pub struct MqGuard<'a, T: Send + Clone> {
+pub struct MqGuard<'a, T: Send + Clone + std::fmt::Debug + tracing::Value> {
     ack: bool,
     queue: sync::Arc<MessageQueue<T>>,
     cell_ptr: std::ptr::NonNull<AckCell<T>>,
     _phantom: std::marker::PhantomData<&'a ()>,
 }
 
-struct MessageQueue<T: Send + Clone> {
+struct MessageQueue<T: Send + Clone + std::fmt::Debug + tracing::Value> {
     ring: std::ptr::NonNull<AckCell<T>>,
-    writter: WriteRegion,
+    senders: sync::atomic::AtomicUsize,
+    writer: WriteRegion,
     reader: ReadRegion,
     cap: usize,
 }
 
 /// An atomic acknowledge cell, use to ensure an element has been read.
-struct AckCell<T: Clone> {
+struct AckCell<T: Clone + std::fmt::Debug + tracing::Value> {
     elem: std::mem::MaybeUninit<T>,
     ack: sync::atomic::AtomicBool,
 }
@@ -55,7 +60,66 @@ struct WriteRegion {
     size: sync::atomic::AtomicUsize,
 }
 
-impl<'a, T: Send + Clone> Drop for MqGuard<'a, T> {
+impl<T: Send + Clone + std::fmt::Debug + tracing::Value> std::fmt::Debug for MqSender<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("MqSender").field("queue", &self.queue).finish()
+    }
+}
+
+impl<T: Send + Clone + std::fmt::Debug + tracing::Value> std::fmt::Debug for MqReceiver<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("MqSender").field("queue", &self.queue).finish()
+    }
+}
+
+impl<'a, T: Send + Clone + std::fmt::Debug + tracing::Value> std::fmt::Debug for MqGuard<'a, T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("MqGuard").field("ack", &self.ack).field("elem", &self.read()).finish()
+    }
+}
+
+impl<T: Send + Clone + std::fmt::Debug + tracing::Value> std::fmt::Debug for MessageQueue<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let senders = self.senders.load(sync::atomic::Ordering::Acquire);
+        f.debug_struct("MessageQueue")
+            .field("senders", &senders)
+            .field("writer", &self.writer)
+            .field("reader", &self.reader)
+            .field("cap", &self.cap)
+            .finish()
+    }
+}
+
+impl std::fmt::Debug for ReadRegion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let start = self.start.load(sync::atomic::Ordering::Acquire);
+        let size = self.start.load(sync::atomic::Ordering::Acquire);
+        sync::atomic::fence(sync::atomic::Ordering::Acquire);
+        f.debug_struct("ReadRegion").field("start", &start).field("size", &size).finish()
+    }
+}
+
+impl std::fmt::Debug for WriteRegion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let start = self.start.load(sync::atomic::Ordering::Acquire);
+        let size = self.start.load(sync::atomic::Ordering::Acquire);
+        sync::atomic::fence(sync::atomic::Ordering::Acquire);
+        f.debug_struct("WriteRegion").field("start", &start).field("size", &size).finish()
+    }
+}
+
+impl<T: Send + Clone + std::fmt::Debug + tracing::Value> Drop for MqSender<T> {
+    fn drop(&mut self) {
+        self.queue.sender_unregister();
+
+        #[cfg(test)]
+        self.wake.notify();
+        #[cfg(not(test))]
+        self.wake.notify_one();
+    }
+}
+
+impl<'a, T: Send + Clone + std::fmt::Debug + tracing::Value> Drop for MqGuard<'a, T> {
     fn drop(&mut self) {
         // If the element was not acknowledged, we add it back to the queue to be picked up again,
         // taking special care to drop the element in the process. We do not need to do this if the
@@ -77,7 +141,7 @@ impl<'a, T: Send + Clone> Drop for MqGuard<'a, T> {
     }
 }
 
-impl<T: Send + Clone> Drop for MessageQueue<T> {
+impl<T: Send + Clone + std::fmt::Debug + tracing::Value> Drop for MessageQueue<T> {
     fn drop(&mut self) {
         self.reader.size.store(0, sync::atomic::Ordering::Release);
 
@@ -93,48 +157,86 @@ impl<T: Send + Clone> Drop for MessageQueue<T> {
     }
 }
 
-pub fn channel<T: Send + Clone>(cap: usize) -> (MqSender<T>, MqReceiver<T>) {
+pub fn channel<T: Send + Clone + std::fmt::Debug + tracing::Value>(cap: usize) -> (MqSender<T>, MqReceiver<T>) {
     let queue_s = sync::Arc::new(MessageQueue::new(cap));
     let queue_r = sync::Arc::clone(&queue_s);
 
-    let wake_s = sync::Arc::new(tokio::sync::Notify::new());
+    let wake_s = sync::Arc::new(Notify::new());
     let wake_r = sync::Arc::clone(&wake_s);
 
-    let sx = MqSender { queue: queue_s, wake: wake_s };
-    let rx = MqReceiver { queue: queue_r, wake: wake_r };
+    let sx = MqSender::new(queue_s, wake_s);
+    let rx = MqReceiver::new(queue_r, wake_r);
 
     (sx, rx)
 }
 
-impl<T: Send + Clone> MqSender<T> {
-    pub async fn send(&self, elem: T) -> Option<T> {
+impl<T: Send + Clone + std::fmt::Debug + tracing::Value> MqSender<T> {
+    fn new(queue: sync::Arc<MessageQueue<T>>, wake: sync::Arc<Notify>) -> Self {
+        queue.sender_register();
+        Self { queue, wake }
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub fn send(&self, elem: T) -> Option<T> {
+        tracing::debug!("Trying to send value");
         match self.queue.write(elem) {
             // Failed to write to the queue. This can happen if the next element right after the
             // write region has been read but not acknowledge yet.
-            Some(elem) => Some(elem),
+            Some(elem) => {
+                tracing::debug!("Failed to send value");
+                Some(elem)
+            }
             None => {
-                self.wake.notify_waiters();
+                tracing::debug!("Value sent successfully");
+
+                #[cfg(test)]
+                self.wake.notify();
+                #[cfg(not(test))]
+                self.wake.notify_one();
+
                 None
             }
         }
     }
 }
 
-impl<T: Send + Clone> MqReceiver<T> {
+impl<T: Send + Clone + std::fmt::Debug + tracing::Value> MqReceiver<T> {
+    fn new(queue: sync::Arc<MessageQueue<T>>, wake: sync::Arc<Notify>) -> Self {
+        Self { queue, wake }
+    }
+
+    #[tracing::instrument(skip(self))]
     pub async fn rcv(&self) -> Option<MqGuard<T>> {
+        tracing::debug!("Trying to receive value");
         loop {
-            // TODO: we still don't have a good way to represent channel closure!
             match self.queue.read() {
-                // FIXME: this will lock indefinitely if all senders are dropped before the
-                // receivers!
-                Some(cell_ptr) => break Some(MqGuard::new(sync::Arc::clone(&self.queue), cell_ptr)),
-                None => self.wake.notified().await,
+                Some(cell_ptr) => {
+                    let guard = MqGuard::new(sync::Arc::clone(&self.queue), cell_ptr);
+                    tracing::debug!(elem = guard.read(), "Received value");
+                    break Some(guard);
+                }
+                None => {
+                    tracing::debug!("Failed to receive value");
+                    if !self.queue.sender_available() && self.queue.reader.size() == 0 {
+                        tracing::debug!("No sender available");
+                        break None;
+                    } else {
+                        tracing::debug!("Waiting for a send");
+
+                        #[cfg(test)]
+                        self.wake.wait();
+                        #[cfg(not(test))]
+                        self.wake.notified().await;
+
+                        tracing::debug!("A send was detected");
+                    }
+                }
             }
         }
     }
 }
 
-impl<'a, T: Send + Clone> MqGuard<'a, T> {
+impl<'a, T: Send + Clone + std::fmt::Debug + tracing::Value> MqGuard<'a, T> {
     pub fn new(queue: sync::Arc<MessageQueue<T>>, cell_ptr: std::ptr::NonNull<AckCell<T>>) -> Self {
         MqGuard { ack: false, queue, cell_ptr, _phantom: std::marker::PhantomData }
     }
@@ -144,6 +246,12 @@ impl<'a, T: Send + Clone> MqGuard<'a, T> {
             let cell = self.cell_ptr.as_ref();
             cell.elem.assume_init_ref().clone()
         }
+    }
+
+    pub fn read_acknowledge(mut self) -> T {
+        let elem = self.read();
+        self.acknowledge();
+        elem
     }
 
     pub fn acknowledge(mut self) {
@@ -197,7 +305,7 @@ impl<'a, T: Send + Clone> MqGuard<'a, T> {
     }
 }
 
-impl<T: Send + Clone> MessageQueue<T> {
+impl<T: Send + Clone + std::fmt::Debug + tracing::Value> MessageQueue<T> {
     fn new(cap: usize) -> Self {
         assert_ne!(std::mem::size_of::<T>(), 0, "T cannot be a ZST");
         assert!(cap > 0, "Tried to create a message queue with a capacity < 1");
@@ -219,10 +327,29 @@ impl<T: Send + Clone> MessageQueue<T> {
             None => std::alloc::handle_alloc_error(layout),
         };
 
+        let senders = sync::atomic::AtomicUsize::new(0);
         let writter = WriteRegion::new(cap);
         let reader = ReadRegion::new();
 
-        Self { ring, cap, writter, reader }
+        Self { ring, cap, senders, writer: writter, reader }
+    }
+
+    #[tracing::instrument(skip(self))]
+    fn sender_register(&self) {
+        let senders = self.senders.fetch_add(1, sync::atomic::Ordering::AcqRel);
+        tracing::debug!(senders = senders + 1, "Increasing sender count");
+        debug_assert_ne!(senders, usize::MAX);
+    }
+
+    #[tracing::instrument(skip(self))]
+    fn sender_unregister(&self) {
+        let senders = self.senders.fetch_sub(1, sync::atomic::Ordering::AcqRel);
+        tracing::debug!(senders = senders - 1, "Decreasing sender count");
+        debug_assert_ne!(senders, 0);
+    }
+
+    fn sender_available(&self) -> bool {
+        self.senders.load(sync::atomic::Ordering::Acquire) > 0
     }
 
     fn read(&self) -> Option<std::ptr::NonNull<AckCell<T>>> {
@@ -230,7 +357,7 @@ impl<T: Send + Clone> MessageQueue<T> {
     }
 
     fn write(&self, elem: T) -> Option<T> {
-        let res = self.writter.write(elem, self.ring, self.cap);
+        let res = self.writer.write(elem, self.ring, self.cap);
         if res.is_none() {
             self.reader.grow(self.cap);
         }
@@ -238,7 +365,7 @@ impl<T: Send + Clone> MessageQueue<T> {
     }
 }
 
-impl<T: Clone> AckCell<T> {
+impl<T: Clone + std::fmt::Debug + tracing::Value> AckCell<T> {
     fn new(elem: T) -> Self {
         Self { elem: std::mem::MaybeUninit::new(elem), ack: sync::atomic::AtomicBool::new(false) }
     }
@@ -257,23 +384,34 @@ impl ReadRegion {
         self.size.load(sync::atomic::Ordering::Acquire)
     }
 
-    fn read<T: Send + Clone>(
+    #[tracing::instrument(skip(self, buf, cap))]
+    fn read<T: Send + Clone + std::fmt::Debug + tracing::Value>(
         &self,
         buf: std::ptr::NonNull<AckCell<T>>,
         cap: usize,
     ) -> Option<std::ptr::NonNull<AckCell<T>>> {
         let size = self.size();
+        tracing::debug!(size, "Trying to read from buffer");
+
         let res = if size == 0 {
             // Note that we do not try to grow the read region in case there is nothing left to
             // read. This is because while cells have and `ack` state to attest if they have been
             // read, we do not store any extra information concerning their write status. Instead,
             // it is the responsibility of the queue to grow the read region whenever it writes a
             // new value.
+            tracing::info!("Failed to read from buffer");
             None
         } else {
             let start = self.start();
-            self.start.store(fast_mod(start + 1, cap), sync::atomic::Ordering::Release);
-            self.size.store(size - 1, sync::atomic::Ordering::Release); // checked above
+            tracing::debug!(start, "Reading from buffer");
+
+            let s = fast_mod(start + 1, cap);
+            self.start.store(s, sync::atomic::Ordering::Release);
+            tracing::debug!(start = s, "Updated read region start");
+
+            let s = size - 1;
+            self.size.store(s, sync::atomic::Ordering::Release); // checked above
+            tracing::debug!(size = s, "Updated read region size");
 
             // Notice how we use the value of `start` before the store. Here the store acts as a
             // locking mechanism, reserving this slot in the queue so that other threads will not
@@ -293,8 +431,12 @@ impl ReadRegion {
         res
     }
 
+    #[tracing::instrument(skip(self, cap))]
     fn grow(&self, cap: usize) {
+        tracing::debug!("Growing read region");
         let size = self.size.fetch_add(1, sync::atomic::Ordering::AcqRel);
+
+        tracing::debug!(size = size + 1, "Growing successful");
         debug_assert_ne!(size, cap);
     }
 }
@@ -313,19 +455,35 @@ impl WriteRegion {
         self.size.load(sync::atomic::Ordering::Acquire)
     }
 
-    fn write<T: Clone>(&self, elem: T, buf: std::ptr::NonNull<AckCell<T>>, cap: usize) -> Option<T> {
+    #[tracing::instrument(skip(self, buf, cap))]
+    fn write<T: Clone + std::fmt::Debug + tracing::Value>(
+        &self,
+        elem: T,
+        buf: std::ptr::NonNull<AckCell<T>>,
+        cap: usize,
+    ) -> Option<T> {
         let size = self.size();
+        tracing::debug!(size, "Trying to write to buffer");
+
         let res = if size == 0 && self.grow(buf, cap).is_err() {
+            tracing::debug!("Failed to grow write region");
             Some(elem)
         } else {
             let start = self.start();
+            tracing::debug!(start, "Writing to buffer");
 
             // size - 1 is checked above and `grow` will increment size by 1 if it succeeds, so
             // whatever happens size > 0
-            self.start.store(fast_mod(start + 1, cap), sync::atomic::Ordering::Release);
-            self.size.store(size - 1, sync::atomic::Ordering::Release);
+            let s = fast_mod(start + 1, cap);
+            self.start.store(s, sync::atomic::Ordering::Release);
+            tracing::debug!(start = s, "Updated write region start");
 
-            unsafe { buf.add(start).write(AckCell::new(elem)) };
+            let s = size - 1;
+            self.size.store(s, sync::atomic::Ordering::Release);
+            tracing::debug!(size = s, "Updated write region size");
+
+            let cell = AckCell::new(elem);
+            unsafe { buf.add(start).write(cell) };
             None
         };
 
@@ -333,7 +491,11 @@ impl WriteRegion {
         res
     }
 
-    fn grow<T: Clone>(&self, buf: std::ptr::NonNull<AckCell<T>>, cap: usize) -> Result<(), &'static str> {
+    fn grow<T: Clone + std::fmt::Debug + tracing::Value>(
+        &self,
+        buf: std::ptr::NonNull<AckCell<T>>,
+        cap: usize,
+    ) -> Result<(), &'static str> {
         // We are indexing the element right AFTER the end of the write region to see if we can
         // overwrite it (ie: it has been read and acknowledged)
         let size = self.size();
@@ -400,4 +562,42 @@ fn fast_mod(n: usize, pow_of_2: usize) -> usize {
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use super::*;
+
+    #[test]
+    fn simple_send() {
+        loom::model(|| {
+            let (sx, rx) = channel(1);
+
+            assert_eq!(sx.queue.writer.start(), 0);
+            assert_eq!(sx.queue.writer.size(), 1);
+            assert_eq!(sx.queue.reader.start(), 0);
+            assert_eq!(sx.queue.reader.size(), 0);
+
+            loom::thread::spawn(move || {
+                assert_matches::assert_matches!(
+                    sx.send(42),
+                    None,
+                    "Failed to send value, message queue is {:#?}",
+                    sx.queue
+                )
+            });
+
+            loom::future::block_on(async move {
+                let guard = rx.rcv().await;
+                assert_matches::assert_matches!(
+                    guard,
+                    Some(guard) => {
+                        assert_eq!(guard.read_acknowledge(), 42);
+                    },
+                    "Failed to acquire acknowledge guard, message queue is {:#?}",
+                    rx.queue
+                );
+
+                let guard = rx.rcv().await;
+                assert!(guard.is_none());
+            })
+        })
+    }
+}
