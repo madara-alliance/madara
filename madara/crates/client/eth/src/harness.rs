346d74fa6e9d8d295c89a0d6e7b96edaf13249b0
Tim B <79199034+timbrinded@users.noreply.github.com>
2025-03-28 10:20:25 +0000

    Improved Error Handling

--- madara/crates/client/eth/src/harness.rs
+++ madara/crates/client/eth/src/harness.rs
@@ -1,6 +1,10 @@
+use crate::error::Error;
 use alloy::node_bindings::{Anvil, AnvilInstance};
 use async_trait::async_trait;
+use reqwest::Client;
+use serde_json::json;
 use tokio::sync::OnceCell;
+use url::Url;
 
 // https://etherscan.io/tx/0xcadb202495cd8adba0d9b382caff907abf755cd42633d23c4988f875f2995d81#eventlog
 // The txn we are referring to it is here ^
@@ -10,7 +14,6 @@ pub const L1_BLOCK_NUMBER: u64 = 20395662;
 pub trait AnvilInstanceInitializer: Sync + Send + 'static {
     async fn init() -> AnvilInstance;
 }
-
 pub struct SharedAnvil<T: AnvilInstanceInitializer> {
     cell: OnceCell<AnvilInstance>,
     _marker: std::marker::PhantomData<T>,
@@ -33,18 +36,19 @@ impl<T: AnvilInstanceInitializer> Default for SharedAnvil<T> {
 }
 
 pub struct MainnetFork;
-
 #[async_trait]
 impl AnvilInstanceInitializer for MainnetFork {
     async fn init() -> AnvilInstance {
-        println!("Initializing Mainnet fork Anvil instance...");
-        tokio::task::spawn_blocking(create_anvil_instance).await.expect("Failed to spawn Anvil instance")
+        create_anvil_instance().await.unwrap_or_else(|err| panic!("Error Instantiating Anvil: {:?}", err))
     }
 }
 
-pub fn create_anvil_instance() -> AnvilInstance {
-    let fork_url = std::env::var("ETH_FORK_URL").expect("ETH_FORK_URL not set");
-    Anvil::new()
+pub async fn create_anvil_instance() -> Result<AnvilInstance, Error> {
+    let fork_url = std::env::var("ETH_FORK_URL").map_err(|_| Error::MissingForkUrl)?;
+    let fork_url = Url::parse(&fork_url).map_err(|_| Error::InvalidForkUrl)?;
+    check_endpoint_alive(fork_url.as_str()).await?;
+
+    let instance = Anvil::new()
         .fork(fork_url)
         .fork_block_number(L1_BLOCK_NUMBER)
         .port(0u16)
@@ -52,5 +56,37 @@ pub fn create_anvil_instance() -> AnvilInstance {
         .timeout(300_000)
         .args(["--fork-retry-backoff", "30000"])
         .try_spawn()
-        .expect("failed to spawn anvil instance")
+        .map_err(Error::NodeSpawnFailed)?;
+
+    Ok(instance)
+}
+
+pub async fn check_endpoint_alive(url: &str) -> Result<bool, Error> {
+    let client = Client::new();
+    let response = client
+        .post(url)
+        .json(&json!({
+            "jsonrpc": "2.0",
+            "method": "eth_chainId",
+            "params": [],
+            "id": 1,
+        }))
+        .timeout(std::time::Duration::from_secs(3))
+        .send()
+        .await;
+
+    match response {
+        Ok(resp) => {
+            if !resp.status().is_success() {
+                if resp.status() == reqwest::StatusCode::TOO_MANY_REQUESTS {
+                    Err(Error::RpcRateLimited)
+                } else {
+                    Err(Error::RpcNotResponsive)
+                }
+            } else {
+                Ok(true)
+            }
+        }
+        Err(_) => Err(Error::RpcNotResponsive),
+    }
 }
