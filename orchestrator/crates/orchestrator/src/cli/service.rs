a5e528c949585a0e5640ecef74f1b3d74f342607
Heemank Verma <heemankv@gmail.com>
2025-04-14 08:55:43 +0530

    update: fix e2e

--- orchestrator/crates/orchestrator/src/cli/service.rs
+++ orchestrator/crates/orchestrator/src/cli/service.rs
@@ -3,11 +3,11 @@ use clap::Args;
 #[derive(Debug, Clone, Args)]
 pub struct ServiceCliArgs {
     /// The maximum block to process.
-    #[arg(env = "MADARA_ORCHESTRATOR_MAX_BLOCK_NO_TO_PROCESS", long, default_value = None)]
+    #[arg(env = "MADARA_ORCHESTRATOR_MAX_BLOCK_NO_TO_PROCESS", long)]
     pub max_block_to_process: Option<u64>,
 
     /// The minimum block to process.
-    #[arg(env = "MADARA_ORCHESTRATOR_MIN_BLOCK_NO_TO_PROCESS", long, default_value = None)]
+    #[arg(env = "MADARA_ORCHESTRATOR_MIN_BLOCK_NO_TO_PROCESS", long)]
     pub min_block_to_process: Option<u64>,
 
     /// The maximum number of SNOS jobs to process concurrently.
