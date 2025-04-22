7022f6e8236bbae2b6402294df3a607160b7f3ce
itsparser <itsparser@gmail.com>
2025-04-22 14:42:24 +0530

    removed the unused code from the code base

--- orchestrator/crates/orchestrator/src/types/jobs/job_updates.rs
+++ orchestrator/crates/orchestrator/src/types/jobs/job_updates.rs
@@ -8,9 +8,7 @@ use serde::Serialize;
 // version and updated_at will always be updated when this object updates the job
 #[derive(Serialize, Debug)]
 pub struct JobItemUpdates {
-    pub job_type: Option<JobType>,
     pub status: Option<JobStatus>,
-    pub external_id: Option<ExternalId>,
     pub metadata: Option<JobMetadata>,
 }
 
@@ -23,20 +21,11 @@ impl Default for JobItemUpdates {
 
 impl JobItemUpdates {
     pub fn new() -> Self {
-        JobItemUpdates { job_type: None, status: None, external_id: None, metadata: None }
-    }
-
-    pub fn update_job_type(mut self, job_type: JobType) -> JobItemUpdates {
-        self.job_type = Some(job_type);
-        self
+        JobItemUpdates { status: None, metadata: None }
     }
 
     pub fn update_status(mut self, status: JobStatus) -> JobItemUpdates {
         self.status = Some(status);
-        self
-    }
-    pub fn update_external_id(mut self, external_id: ExternalId) -> JobItemUpdates {
-        self.external_id = Some(external_id);
         self
     }
     pub fn update_metadata(mut self, metadata: JobMetadata) -> JobItemUpdates {
