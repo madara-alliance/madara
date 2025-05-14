73679923181d47a7eb737a36a4e56236cca15e9c
Prakhar Pandey <prkpandey942@gmail.com>
2025-05-14 19:10:45 +0530

    remove: unused constants in constant.rs

--- orchestrator/crates/orchestrator/src/types/constant.rs
+++ orchestrator/crates/orchestrator/src/types/constant.rs
@@ -3,5 +3,3 @@ pub const SNOS_OUTPUT_FILE_NAME: &str = "snos_output.json";
 pub const PROGRAM_OUTPUT_FILE_NAME: &str = "program_output.txt";
 pub const CAIRO_PIE_FILE_NAME: &str = "cairo_pie.zip";
 pub const S3_STATE_UPDATE_DIR: &str = "state_update";
-// TODO: Remove this constant when `assign_batch_to_block` method is updated
-pub const MAX_BATCH_SIZE: u64 = 50;
