347b4d60309407040e0b9f6eb52171ab739c7ff4
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-05-14 11:33:39 +0200

    build(deps): code to use new artifacts

--- orchestrator/crates/orchestrator/src/utils/mod.rs
+++ orchestrator/crates/orchestrator/src/utils/mod.rs
@@ -5,4 +5,4 @@ pub mod metrics;
 
 /// TODO: This is super Awkward to have this code here
 /// but will try to remove this and move it to the config from the root path
-pub const COMPILED_OS: &[u8] = include_bytes!("../../../../build/os_latest.json");
+pub const COMPILED_OS: &[u8] = include_bytes!("../../../../../build_artifacts/cairo_lang/os_latest.json");
