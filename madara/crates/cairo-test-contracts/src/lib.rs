42958c9803b57e6a94b48a51f3f2d4ad77d92907
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-05-16 11:28:11 +0200

    fix(build): local contract artifacts

--- madara/crates/cairo-test-contracts/src/lib.rs
+++ madara/crates/cairo-test-contracts/src/lib.rs
@@ -2,8 +2,8 @@
 //! tests need to be put in the `cairo-artifacts` folder at the root of the project`.
 
 pub const TEST_CONTRACT_SIERRA: &[u8] =
-    include_bytes!("../../../../cairo/target/dev/madara_contracts_TestContract.contract_class.json");
+    include_bytes!("../../../../build_artifacts/cairo/madara_contracts_TestContract.contract_class.json");
 pub const APPCHAIN_CONTRACT_SIERRA: &[u8] =
-    include_bytes!("../../../../cairo/target/dev/madara_contracts_StateUpdateContract.contract_class.json");
+    include_bytes!("../../../../build_artifacts/cairo/madara_contracts_StateUpdateContract.contract_class.json");
 pub const MESSAGING_CONTRACT_SIERRA: &[u8] =
-    include_bytes!("../../../../cairo/target/dev/madara_contracts_MessagingContract.contract_class.json");
+    include_bytes!("../../../../build_artifacts/cairo/madara_contracts_MessagingContract.contract_class.json");
