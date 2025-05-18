802a8473718edf51c6380c52b01bf51e7a2f3fbf
itsparser <itsparser@gmail.com>
2025-05-18 22:53:23 +0530

    feat(queue): add max_retry and retry_delay to QueueConfig for enhanced job processing control
    - Introduced `max_retry` and `retry_delay` fields to the `QueueConfig` struct to allow for configurable retry logic.
    - Updated existing queue configurations to utilize the new fields, specifying retry limits and delays for various job types.
    - Ensured backward compatibility by setting default values for existing configurations.

--- orchestrator/crates/orchestrator/src/setup/queue.rs
+++ orchestrator/crates/orchestrator/src/setup/queue.rs
@@ -12,52 +12,96 @@ pub struct DlqConfig {
 pub struct QueueConfig {
     pub name: QueueType,
     pub visibility_timeout: u32,
+    pub max_retry: Option<u16>,         // max number of retries
+    pub retry_delay: Option<u32>,       // in seconds
     pub dlq_config: Option<DlqConfig>,
 }
 
 lazy_static! {
     pub static ref QUEUES: Vec<QueueConfig> = vec![
-        QueueConfig { name: QueueType::JobHandleFailure, visibility_timeout: 300, dlq_config: None },
+        QueueConfig {
+            name: QueueType::JobHandleFailure,
+            visibility_timeout: 300,
+            dlq_config: None,
+            max_retry: None,
+            retry_delay: None,
+        },
         QueueConfig {
             name: QueueType::SnosJobProcessing,
             visibility_timeout: 300,
+            max_retry: Some(1),
+            retry_delay: Some(1),
             dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
         },
         QueueConfig {
             name: QueueType::SnosJobVerification,
             visibility_timeout: 300,
+            max_retry: Some(1),
+            retry_delay: Some(1),
             dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
         },
         QueueConfig {
             name: QueueType::ProvingJobProcessing,
             visibility_timeout: 300,
+            max_retry: Some(2),
+            retry_delay: Some(30),
             dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
         },
         QueueConfig {
             name: QueueType::ProvingJobVerification,
             visibility_timeout: 300,
+            max_retry: Some(300),
+            retry_delay: Some(30),
+            dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
+        },
+        QueueConfig {
+            name: QueueType::ProofRegistrationJobProcessing,
+            visibility_timeout: 300,
+            max_retry: Some(2),
+            retry_delay: Some(30),
+            dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
+        },
+        QueueConfig {
+            name: QueueType::ProofRegistrationJobVerification,
+            visibility_timeout: 300,
+            max_retry: Some(2),
+            retry_delay: Some(30),
             dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
         },
         QueueConfig {
             name: QueueType::DataSubmissionJobProcessing,
             visibility_timeout: 300,
+            max_retry: Some(1),
+            retry_delay: Some(60),
             dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
         },
         QueueConfig {
             name: QueueType::DataSubmissionJobVerification,
             visibility_timeout: 300,
+            max_retry: Some(3),
+            retry_delay: Some(60),
             dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
         },
         QueueConfig {
             name: QueueType::UpdateStateJobProcessing,
             visibility_timeout: 900,
+            max_retry: Some(1),
+            retry_delay: Some(60),
             dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
         },
         QueueConfig {
             name: QueueType::UpdateStateJobVerification,
             visibility_timeout: 300,
+            max_retry: Some(10),
+            retry_delay: Some(60),
             dlq_config: Some(DlqConfig { max_receive_count: 5, dlq_name: QueueType::JobHandleFailure })
         },
-        QueueConfig { name: QueueType::WorkerTrigger, visibility_timeout: 300, dlq_config: None },
+        QueueConfig {
+            name: QueueType::WorkerTrigger,
+            visibility_timeout: 300,
+            max_retry: None,
+            retry_delay: None,
+            dlq_config: None
+        },
     ];
 }
