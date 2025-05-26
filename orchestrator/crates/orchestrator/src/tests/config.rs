74e0847f31cfbd0a3b897f0336ca4929cf8e2c80
Heemank Verma <heemankv@gmail.com>
2025-05-26 19:12:51 +0530

    fix: lint

--- orchestrator/crates/orchestrator/src/tests/config.rs
+++ orchestrator/crates/orchestrator/src/tests/config.rs
@@ -507,7 +507,9 @@ pub(crate) fn get_env_params() -> EnvParams {
     });
 
     let alert_params = AlertArgs {
-        alert_identifier: AWSResourceIdentifier::Name(get_env_var_or_panic("MADARA_ORCHESTRATOR_AWS_SNS_TOPIC_IDENTIFIER")),
+        alert_identifier: AWSResourceIdentifier::Name(get_env_var_or_panic(
+            "MADARA_ORCHESTRATOR_AWS_SNS_TOPIC_IDENTIFIER",
+        )),
     };
 
     let settlement_params = SettlementConfig::Ethereum(EthereumSettlementValidatedArgs {
