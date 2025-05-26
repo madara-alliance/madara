dd0e12c7002ececee49a29f1da90a21430caf267
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-05-26 19:06:01 +0200

    build(orchestrator): invalid artifact path

--- orchestrator/crates/settlement-clients/starknet/build.rs
+++ orchestrator/crates/settlement-clients/starknet/build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    build_version::get_or_compile_artifacts(1).expect("Failed to load artifacts");
+    build_version::get_or_compile_artifacts(4).expect("Failed to load artifacts");
 }
