8a8050e450722e0db57a011cc6a3a156d421f18b
Trantorian1 <114066155+Trantorian1@users.noreply.github.com>
2025-05-30 12:38:17 +0200

    refactor(ci): optimize CI run times (#643)

--- madara/crates/cairo-test-contracts/build.rs
+++ madara/crates/cairo-test-contracts/build.rs
@@ -1,23 +1,3 @@
-use std::path::PathBuf;
-
 fn main() {
-    let cairo_project_dir = PathBuf::from("../../../cairo/").canonicalize().unwrap();
-    println!("cargo::rerun-if-changed={}", cairo_project_dir.join("src").display());
-    println!("cargo::rerun-if-changed={}", cairo_project_dir.join("Scarb.toml").display());
-    // Note: scarb build updates the Scarb.lock file, and there is no `--locked` arg to avoid
-    //  that, so we can't use rerun-if-change on it :/
-    // println!("cargo::rerun-if-changed={}", cairo_project_dir.join("Scarb.lock").display());
-    let exit_status = std::process::Command::new("scarb")
-        .current_dir(cairo_project_dir)
-        .args(["build"])
-        .spawn()
-        .expect("Scarb needs to be installed in order to compile the test contracts")
-        .wait()
-        .unwrap();
-    if !exit_status.success() {
-        panic!(
-            "`scarb build` command returned with a non-zero exit code: {}",
-            exit_status.code().expect("`scarb build` command was stopped by a signal`")
-        );
-    }
+    build_version::get_or_compile_artifacts(3).expect("Failed to load artifacts");
 }
