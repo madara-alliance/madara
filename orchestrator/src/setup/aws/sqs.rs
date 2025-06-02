8fa1189264b7746267dbd37ddb1272361048a793
itsparser <itsparser@gmail.com>
2025-06-03 02:48:38 +0530

    refactor: rename q_control module to queue_control and update related references

--- orchestrator/src/setup/aws/sqs.rs
+++ orchestrator/src/setup/aws/sqs.rs
@@ -3,7 +3,7 @@ use crate::{
     core::client::queue::sqs::SQS,
     core::cloud::CloudProvider,
     core::traits::resource::Resource,
-    types::{params::QueueArgs, q_control::QUEUES},
+    types::{params::QueueArgs, queue_control::QUEUES},
     OrchestratorError, OrchestratorResult,
 };
 use async_trait::async_trait;
