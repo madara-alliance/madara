43973b39d641d2f40414b4fd41747751f9e90f92
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-06-09 11:23:59 +0200

    feat(ws): started work on `subscribePendingTransaction`

--- madara/crates/primitives/rpc/src/v0_8_1/starknet_ws_api.rs
+++ madara/crates/primitives/rpc/src/v0_8_1/starknet_ws_api.rs
@@ -3,3 +3,9 @@ pub struct TxnStatus {
     pub transaction_hash: starknet_types_core::felt::Felt,
     pub status: crate::v0_7_1::TxnStatus,
 }
+
+#[derive(Eq, Hash, PartialEq, serde::Serialize, serde::Deserialize, Clone, Debug)]
+pub enum PendingTxnInfo {
+    Hash(starknet_types_core::felt::Felt),
+    Full(crate::v0_7_1::Txn),
+}
