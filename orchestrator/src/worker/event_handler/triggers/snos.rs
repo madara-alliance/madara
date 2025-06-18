9dff8ed919382d219032293a80537590d2e6fb90
Prakhar Pandey <prkpandey942@gmail.com>
2025-06-18 16:52:34 +0530

    hotfix: make full_output as true always
    proper fix will involve taking this value from the config

--- orchestrator/src/worker/event_handler/triggers/snos.rs
+++ orchestrator/src/worker/event_handler/triggers/snos.rs
@@ -56,7 +56,7 @@ impl JobTrigger for SnosJobTrigger {
                 common: CommonMetadata::default(),
                 specific: JobSpecificMetadata::Snos(SnosMetadata {
                     block_number: block_num,
-                    full_output: false,
+                    full_output: true,
                     // Set the storage paths using block number
                     cairo_pie_path: Some(format!("{}/{}", block_num, CAIRO_PIE_FILE_NAME)),
                     snos_output_path: Some(format!("{}/{}", block_num, SNOS_OUTPUT_FILE_NAME)),
