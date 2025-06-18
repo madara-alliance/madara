4dd91e85bdc0d6c5a93839ac41c74e9cca09b568
jbcaron <jbcaron.dev@gmail.com>
2025-06-18 11:01:03 +0200

    Revert "fix: add deterministic constructor hasher for bloom filter"
    This reverts commit 27c4f3ec3c2caa9e0eb32c448b3ee43068a5734b.

--- madara/crates/primitives/bloom_filter/src/tests/utils.rs
+++ madara/crates/primitives/bloom_filter/src/tests/utils.rs
@@ -1,6 +1,6 @@
 use std::hash::Hasher;
 
-use crate::{filter::DeterministicBuildHasher, AtomicBitStore, BloomFilter};
+use crate::{AtomicBitStore, BloomFilter};
 
 // Constants for Bloom filter configuration
 // Number of hash functions used in the Bloom filter.
@@ -14,7 +14,7 @@ const BITS_PER_ELEMENT: f64 = 9.6;
 pub const FALSE_POSITIF_RATE: f64 = 0.01;
 
 /// Helper function to create a Bloom filter with the given number of elements
-pub fn create_filter<H: Hasher + DeterministicBuildHasher>(nb_elem: u64) -> BloomFilter<H, AtomicBitStore> {
+pub fn create_filter<H: Hasher + Default>(nb_elem: u64) -> BloomFilter<H, AtomicBitStore> {
     let size = (nb_elem as f64 * BITS_PER_ELEMENT).ceil() as u64;
     BloomFilter::new(size, HASH_COUNT)
 }
