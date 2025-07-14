2e336102085f3122f7018c78de59d1c737b2e133
Prakhar Pandey <prkpandey942@gmail.com>
2025-07-15 01:32:06 +0530

    add: test for functions converting stateless compressed data to blob

--- orchestrator/src/tests/compression/mod.rs
+++ orchestrator/src/tests/compression/mod.rs
@@ -1,13 +1,14 @@
-use crate::compression::blob::state_update_to_blob_data;
+use crate::compression::blob::{convert_felt_vec_to_blob_data, state_update_to_blob_data};
 use crate::compression::squash::squash_state_updates;
 use crate::compression::stateful::compress as stateful_compress;
 use crate::compression::stateless::compress as stateless_compress;
 use crate::core::config::StarknetVersion;
 use crate::tests::jobs::batching_job::convert_biguints_to_felts;
 use crate::tests::utils::{
-    build_test_config_with_real_provider, read_biguint_from_file, read_state_update_from_file,
+    build_test_config_with_real_provider, read_biguint_from_file, read_file_to_string, read_state_update_from_file,
     read_state_updates_vec_from_file,
 };
+use crate::worker::utils::biguint_vec_to_u8_vec;
 use color_eyre::eyre::Result;
 use rstest::*;
 use std::io::Read;
@@ -80,6 +81,25 @@ async fn test_stateless_compression(#[case] version: &str) -> Result<()> {
     let compressed_state_update = stateless_compress(&vec_felts)?;
 
     assert_eq!(compressed_state_update, expected_compressed_state_update);
+
+    Ok(())
+}
+
+#[rstest]
+#[tokio::test]
+async fn test_felt_vec_to_blob_data() -> Result<()> {
+    let vec_felts = convert_biguints_to_felts(&read_biguint_from_file(&format!(
+        "{}/src/tests/artifacts/stateless_compressed_state_update_789878_789900.json",
+        env!("CARGO_MANIFEST_DIR")
+    ))?)?;
+
+    let blobs = convert_felt_vec_to_blob_data(&vec_felts)?;
+    for (index, blob) in blobs.iter().enumerate() {
+        assert_eq!(
+            hex::encode(biguint_vec_to_u8_vec(blob.as_slice())),
+            read_file_to_string(&format!("{}/src/tests/artifacts/blob/{}.txt", env!("CARGO_MANIFEST_DIR"), index + 1))?
+        );
+    }
 
     Ok(())
 }
