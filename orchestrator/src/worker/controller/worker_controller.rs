f703c6221a3ed7f7805b32a3c090f2bb7cbcf1c2
Prakhar Pandey <prkpandey942@gmail.com>
2025-07-22 16:01:25 +0530

    add: aggregator queues

--- orchestrator/src/worker/controller/worker_controller.rs
+++ orchestrator/src/worker/controller/worker_controller.rs
@@ -71,12 +71,10 @@ impl WorkerController {
         vec![
             QueueType::SnosJobProcessing,
             QueueType::ProvingJobProcessing,
-            QueueType::DataSubmissionJobProcessing,
             QueueType::AggregatorJobProcessing,
             QueueType::UpdateStateJobProcessing,
             QueueType::SnosJobVerification,
             QueueType::ProvingJobVerification,
-            QueueType::DataSubmissionJobVerification,
             QueueType::AggregatorJobVerification,
             QueueType::UpdateStateJobVerification,
             QueueType::WorkerTrigger,
