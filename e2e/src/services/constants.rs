5f0e56c2142b0929a5e234407bb38462acc1ecea
Heemank Verma <heemankv@gmail.com>
2025-07-23 15:44:52 +0530

    fixes: update constants

--- e2e/src/services/constants.rs
+++ e2e/src/services/constants.rs
@@ -1,3 +1,7 @@
+// =============================================================================
+// CONSTANTS
+// =============================================================================
+
 pub const ANVIL_DEFAULT_DATABASE_NAME: &str = "anvil.json";
 pub const DEFAULT_BOOTSTRAPPER_BINARY: &str = "../target/release/bootstrapper";
 pub const DEFAULT_BOOTSTRAPPER_CONFIG: &str = "./config/bootstrapper.json";
