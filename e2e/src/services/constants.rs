984196d5a79addff8885eaa96a3e62eabbeec596
Heemank Verma <heemankv@gmail.com>
2025-07-23 15:32:26 +0530

    fixes: update constants

--- e2e/src/services/constants.rs
+++ e2e/src/services/constants.rs
@@ -1,3 +1,6 @@
 pub const ANVIL_DEFAULT_DATABASE_NAME: &str = "anvil.json";
+pub const DEFAULT_BOOTSTRAPPER_BINARY: &str = "../target/release/bootstrapper";
+pub const DEFAULT_BOOTSTRAPPER_CONFIG: &str = "./config/bootstrapper.json";
+pub const BOOTSTRAPPER_DEFAULT_ADDRESS_PATH: &str = "addresses.json";
 pub const DEFAULT_SERVICE_HOST: &str = "127.0.0.1";
 pub const DEFAULT_MONGODB_DIR: &str = "data";
