85d652b1242a58e0c29ac52dff7f8b8add281ecd
Heemank Verma <heemankv@gmail.com>
2025-08-06 11:51:41 +0530

    Working Deposit ETH + ERC20

--- e2e/src/setup/config.rs
+++ e2e/src/setup/config.rs
@@ -430,6 +430,11 @@ impl SetupConfigBuilder {
             .logs((false, true))
             .build();
 
+        // Setting some envs
+        std::env::set_var("ETH_RPC", anvil_config.endpoint().as_str());
+        std::env::set_var("ETH_PRIVATE_KEY", ANVIL_PRIVATE_KEY);
+        std::env::set_var("ROLLUP_SEQ_URL", madara_config.rpc_endpoint().to_string());
+
         let sconfig = self
             .anvil_config(anvil_config)
             .madara_config(test_config.madara_config)
