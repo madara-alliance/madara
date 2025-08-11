940980c2e06526d17d1e756d97bd05222e6feabe
Prakhar Pandey <prkpandey942@gmail.com>
2025-08-11 22:38:01 +0530

    fix: state update job tests

--- orchestrator/src/tests/jobs/state_update_job/mod.rs
+++ orchestrator/src/tests/jobs/state_update_job/mod.rs
@@ -4,7 +4,6 @@ use std::path::PathBuf;
 
 use assert_matches::assert_matches;
 use bytes::Bytes;
-use color_eyre::eyre::eyre;
 use httpmock::prelude::*;
 use lazy_static::lazy_static;
 use mockall::predicate::{always, eq};
@@ -20,7 +19,9 @@ use crate::error::job::state_update::StateUpdateError;
 use crate::error::job::JobError;
 use crate::tests::common::default_job_item;
 use crate::tests::config::{ConfigType, TestConfigBuilder};
-use crate::types::constant::{BLOB_DATA_FILE_NAME, PROGRAM_OUTPUT_FILE_NAME, SNOS_OUTPUT_FILE_NAME};
+use crate::types::constant::{
+    BLOB_DATA_FILE_NAME, PROGRAM_OUTPUT_FILE_NAME, SNOS_OUTPUT_FILE_NAME, STORAGE_ARTIFACTS_DIR, STORAGE_BLOB_DIR,
+};
 use crate::types::jobs::metadata::{
     CommonMetadata, JobMetadata, JobSpecificMetadata, SettlementContext, SettlementContextData, StateUpdateMetadata,
 };
@@ -62,12 +63,12 @@ async fn test_process_job_attempt_not_present_fails() {
 
 // TODO : make this test work
 #[rstest]
-#[case(None, String::from("651053,651054,651055"), 0)]
-#[case(Some(651054), String::from("651053,651054,651055"), 1)]
+#[case(None, vec![651053, 651054, 651055], 0)]
+#[case(Some(651054), vec![651053, 651054, 651055], 1)]
 #[tokio::test]
 async fn test_process_job_works(
     #[case] failed_block_number: Option<u64>,
-    #[case] blocks_to_process: String,
+    #[case] blocks_to_process: Vec<u64>,
     #[case] processing_start_index: u8,
 ) {
     dotenvy::from_filename_override("../.env.test").expect("Failed to load the .env file");
@@ -75,13 +76,11 @@ async fn test_process_job_works(
     // Mocking the settlement client.
     let mut settlement_client = MockSettlementClient::new();
 
-    let block_numbers: Vec<u64> = parse_block_numbers(&blocks_to_process).unwrap();
-
     // This must be the last block number and should be returned as an output from the process job.
-    let last_block_number = block_numbers[block_numbers.len() - 1];
+    let last_block_number = blocks_to_process[blocks_to_process.len() - 1];
 
     // Adding expectations for each block number to be called by settlement client.
-    for block in block_numbers.iter().skip(processing_start_index as usize) {
+    for block in blocks_to_process.iter().skip(processing_start_index as usize) {
         // Getting the blob data from file.
         let blob_data = fs::read_to_string(
             CURRENT_PATH.join(format!("src/tests/jobs/state_update_job/test_data/{}/{}", block, BLOB_DATA_FILE_NAME)),
@@ -105,6 +104,7 @@ async fn test_process_job_works(
             .times(1)
             .returning(|_, _, _| Ok("0xbeef".to_string()));
     }
+
     settlement_client.expect_get_last_settled_block().with().returning(move || Ok(Some(651052)));
     // Setting random nonce
     settlement_client.expect_get_nonce().with().returning(move || Ok(2));
@@ -124,9 +124,9 @@ async fn test_process_job_works(
     let mut program_output_paths = Vec::new();
     let mut blob_data_paths = Vec::new();
 
-    for block in block_numbers.iter() {
+    for block in blocks_to_process.iter() {
         // Getting the blob data from file.
-        let blob_data_key = block.to_string() + "/" + BLOB_DATA_FILE_NAME;
+        let blob_data_key = format!("{}/batch/{}", STORAGE_BLOB_DIR, block);
         let blob_data = fs::read_to_string(
             CURRENT_PATH.join(format!("src/tests/jobs/state_update_job/test_data/{}/{}", block, BLOB_DATA_FILE_NAME)),
         )
@@ -134,14 +134,14 @@ async fn test_process_job_works(
         let blob_data_vec = hex_string_to_u8_vec(&blob_data).unwrap();
 
         // Getting the snos data from file.
-        let snos_output_key = block.to_string() + "/" + SNOS_OUTPUT_FILE_NAME;
+        let snos_output_key = format!("{}/batch/{}/{}", STORAGE_ARTIFACTS_DIR, block, SNOS_OUTPUT_FILE_NAME);
         let snos_output_data = fs::read_to_string(
             CURRENT_PATH.join(format!("src/tests/jobs/state_update_job/test_data/{}/{}", block, SNOS_OUTPUT_FILE_NAME)),
         )
         .unwrap();
 
         // Getting the program output data from file.
-        let program_output_key = block.to_string() + "/" + PROGRAM_OUTPUT_FILE_NAME;
+        let program_output_key = format!("{}/batch/{}/{}", STORAGE_ARTIFACTS_DIR, block, PROGRAM_OUTPUT_FILE_NAME);
         let program_output_data = read_file_to_vec_u8_32(
             CURRENT_PATH
                 .join(format!("src/tests/jobs/state_update_job/test_data/{}/{}", block, PROGRAM_OUTPUT_FILE_NAME))
@@ -152,7 +152,7 @@ async fn test_process_job_works(
         let program_output_data_serialized = bincode::serialize(&program_output_data).unwrap();
 
         storage_client.put_data(Bytes::from(snos_output_data), &snos_output_key).await.unwrap();
-        storage_client.put_data(Bytes::from(blob_data_vec), &blob_data_key).await.unwrap();
+        storage_client.put_data(Bytes::from(blob_data_vec), &format!("{}/1.txt", blob_data_key)).await.unwrap();
         storage_client.put_data(Bytes::from(program_output_data_serialized), &program_output_key).await.unwrap();
 
         // Add paths to our vectors for metadata
@@ -170,7 +170,7 @@ async fn test_process_job_works(
             blob_data_paths,
             tx_hashes: Vec::new(), // Start with empty tx_hashes, they'll be populated during processing
             context: SettlementContext::Block(SettlementContextData {
-                to_settle: block_numbers.clone(),
+                to_settle: blocks_to_process.clone(),
                 last_failed: failed_block_number,
             }),
         }),
@@ -221,18 +221,19 @@ async fn create_job_works() {
 #[rstest]
 #[tokio::test]
 async fn process_job_works_unit_test() {
+    // Mocking settlement and storage client
     let mut settlement_client = MockSettlementClient::new();
     let mut storage_client = MockStorageClient::new();
 
     // Mock the latest block settled
     settlement_client.expect_get_last_settled_block().returning(|| Ok(Some(651052_u64)));
 
-    // TODO: have tests for update_state_calldata, only kzg for now
     let block_numbers = ["651053", "651054", "651055", "651056"];
     for block_no in block_numbers {
         let _state_diff: Vec<u8> = load_state_diff_file(block_no.parse::<u64>().unwrap()).await;
 
-        let snos_output_key = block_no.to_owned() + "/" + SNOS_OUTPUT_FILE_NAME;
+        // Setting expectation for SNOS output
+        let snos_output_key = format!("{}/batch/{}/{}", STORAGE_ARTIFACTS_DIR, block_no, SNOS_OUTPUT_FILE_NAME);
         let snos_output_data = fs::read_to_string(
             CURRENT_PATH
                 .join(format!("src/tests/jobs/state_update_job/test_data/{}/{}", block_no, SNOS_OUTPUT_FILE_NAME)),
@@ -243,7 +244,8 @@ async fn process_job_works_unit_test() {
             .with(eq(snos_output_key.clone()))
             .returning(move |_| Ok(Bytes::from(snos_output_data.clone())));
 
-        let blob_data_key = block_no.to_owned() + "/" + BLOB_DATA_FILE_NAME;
+        // Setting expectation for blob data
+        let blob_data_key = format!("{}/batch/{}", STORAGE_BLOB_DIR, block_no);
         let blob_data = fs::read_to_string(
             CURRENT_PATH
                 .join(format!("src/tests/jobs/state_update_job/test_data/{}/{}", block_no, BLOB_DATA_FILE_NAME)),
@@ -253,17 +255,15 @@ async fn process_job_works_unit_test() {
         let blob_data_vec_clone = blob_data_vec.clone();
         storage_client
             .expect_get_data()
-            .with(eq(blob_data_key.clone()))
+            .with(eq(format!("{}/1.txt", blob_data_key)))
             .returning(move |_| Ok(Bytes::from(blob_data_vec.clone())));
+        storage_client
+            .expect_list_files_in_dir()
+            .with(eq(blob_data_key.clone()))
+            .returning(move |_| Ok(vec![format!("{}/1.txt", blob_data_key)]));
 
-        let x_0_key = block_no.to_owned() + "/" + X_0_FILE_NAME;
-        let x_0 = fs::read_to_string(
-            CURRENT_PATH.join(format!("src/tests/jobs/state_update_job/test_data/{}/{}", block_no, X_0_FILE_NAME)),
-        )
-        .expect("Failed to read the blob data txt file");
-        storage_client.expect_get_data().with(eq(x_0_key)).returning(move |_| Ok(Bytes::from(x_0.clone())));
-
-        let program_output_key = block_no.to_owned() + "/" + PROGRAM_OUTPUT_FILE_NAME;
+        // Setting expectation for program output
+        let program_output_key = format!("{}/batch/{}/{}", STORAGE_ARTIFACTS_DIR, block_no, PROGRAM_OUTPUT_FILE_NAME);
         let program_output = read_file_to_vec_u8_32(
             CURRENT_PATH
                 .join(format!("src/tests/jobs/state_update_job/test_data/{}/{}", block_no, PROGRAM_OUTPUT_FILE_NAME))
@@ -277,11 +277,12 @@ async fn process_job_works_unit_test() {
             .with(eq(program_output_key.clone()))
             .returning(move |_| Ok(Bytes::from(bincode::serialize(&program_output).unwrap())));
 
+        // Setting expectation for nonce
         settlement_client.expect_get_nonce().returning(|| Ok(1));
 
+        // Setting expectation for update_state_with_blobs
         let deserialized_program_output: Vec<[u8; 32]> =
             bincode::deserialize(&bincode::serialize(&program_output_clone).unwrap()).unwrap();
-
         settlement_client
             .expect_update_state_with_blobs()
             .with(eq(deserialized_program_output), eq(vec![blob_data_vec_clone]), always())
@@ -300,13 +301,16 @@ async fn process_job_works_unit_test() {
         specific: JobSpecificMetadata::StateUpdate(StateUpdateMetadata {
             snos_output_paths: block_numbers
                 .iter()
-                .map(|block| format!("{}/{}", block, SNOS_OUTPUT_FILE_NAME))
+                .map(|block| format!("{}/batch/{}/{}", STORAGE_ARTIFACTS_DIR, block, SNOS_OUTPUT_FILE_NAME))
+                .collect(),
+            blob_data_paths: block_numbers
+                .iter()
+                .map(|block| format!("{}/batch/{}", STORAGE_BLOB_DIR, block))
                 .collect(),
             program_output_paths: block_numbers
                 .iter()
-                .map(|block| format!("{}/{}", block, PROGRAM_OUTPUT_FILE_NAME))
+                .map(|block| format!("{}/batch/{}/{}", STORAGE_ARTIFACTS_DIR, block, PROGRAM_OUTPUT_FILE_NAME))
                 .collect(),
-            blob_data_paths: block_numbers.iter().map(|block| format!("{}/{}", block, BLOB_DATA_FILE_NAME)).collect(),
             tx_hashes: vec![],
             context: SettlementContext::Block(SettlementContextData {
                 to_settle: block_numbers.iter().map(|b| b.parse::<u64>().unwrap()).collect(),
@@ -438,16 +442,6 @@ async fn load_state_diff_file(block_no: u64) -> Vec<u8> {
     let file_path = format!("src/tests/jobs/state_update_job/test_data/{}/{}", block_no, BLOB_DATA_FILE_NAME);
     let file_data = fs::read_to_string(file_path).expect("Unable to read blob_data.txt").replace("0x", "");
     hex_string_to_u8_vec(&file_data).unwrap()
-}
-
-fn parse_block_numbers(blocks_to_settle: &str) -> color_eyre::Result<Vec<u64>> {
-    let sanitized_blocks = blocks_to_settle.replace(' ', "");
-    let block_numbers: Vec<u64> = sanitized_blocks
-        .split(',')
-        .map(|block_no| block_no.parse::<u64>())
-        .collect::<color_eyre::Result<Vec<u64>, _>>()
-        .map_err(|e| eyre!("Block numbers to settle list is not correctly formatted: {e}"))?;
-    Ok(block_numbers)
 }
 
 fn read_file_to_vec_u8_32(filename: &str) -> std::io::Result<Vec<[u8; 32]>> {
