bc1c71cea98cb5971a2a6be432bf08909cf5bded
Prakhar Pandey <prkpandey942@gmail.com>
2025-08-27 11:57:13 +0530

    fix: enhance logging with JSON format, span and thread details for improved traceability

--- orchestrator/src/utils/instrument.rs
+++ orchestrator/src/utils/instrument.rs
@@ -37,15 +37,22 @@ impl OrchestratorInstrumentation {
                 Ok(Self { otel_config: config.clone(), meter_provider: None })
             }
             Some(ref endpoint) => {
-                let tracing_subscriber = tracing_subscriber::registry()
-                    .with(tracing_subscriber::fmt::layer())
-                    .with(EnvFilter::from_default_env());
-
                 let meter_provider = Self::instrument_metric_provider(config, endpoint)?;
                 let tracer = Self::instrument_tracer_provider(config, endpoint)?;
                 let logger = Self::instrument_logger_provider(config, endpoint)?;
 
-                let subscriber = tracing_subscriber
+                let subscriber = tracing_subscriber::registry()
+                    .with(
+                        tracing_subscriber::fmt::layer()
+                            .json()
+                            .with_current_span(true)
+                            .with_span_list(true)
+                            .with_target(true)
+                            .with_thread_ids(true)
+                            .with_file(true)
+                            .with_line_number(true),
+                    )
+                    .with(EnvFilter::from_default_env())
                     .with(OpenTelemetryLayer::new(tracer))
                     .with(OpenTelemetryTracingBridge::new(&logger));
 
