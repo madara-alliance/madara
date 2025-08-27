f59fe025fa6e083b01d1a9797266ccbec167fed0
Mehul Chauhan <imehul24@gmail.com>
2025-08-27 13:04:25 +0530

    sleep acc to build order

--- bootstrapper-v2/build.rs
+++ bootstrapper-v2/build.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
+    std::thread::sleep(std::time::Duration::from_secs(300));
     build_version::get_or_compile_artifacts(1).expect("Failed to load artifacts");
 
     const BOOTSTRAPPER_DIR: &str = "../build-artifacts/bootstrapper";
