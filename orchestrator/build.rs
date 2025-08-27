118499ddc591cdd30a4e2aebf70d460c4479b6a5
Mehul Chauhan <imehul24@gmail.com>
2025-08-27 14:26:38 +0530

    increase sleep

--- orchestrator/build.rs
+++ orchestrator/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    std::thread::sleep(std::time::Duration::from_secs(600));
+    std::thread::sleep(std::time::Duration::from_secs(1500));
     build_version::get_or_compile_artifacts(1).expect("Failed to load artifacts");
 }
