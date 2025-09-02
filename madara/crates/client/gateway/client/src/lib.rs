6a5f3f5bfd27e1f439b026743bfad0e36eccb009
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-09-02 08:30:59 +0200

    docs(fgw): started feeder docs

--- madara/crates/client/gateway/client/src/lib.rs
+++ madara/crates/client/gateway/client/src/lib.rs
@@ -1,3 +1,14 @@
+//! The **Feeder Gateway**, which you will sometimes see referred to as _fgw_, is an unspecified
+//! endpoint of a Starknet node. It is used for synchronization during block sync from a centralized
+//! source, predating Starknet's upgrade to p2p block sync.
+//!
+//! Despite this, the fgw has never been properly specified. Parity with mainnet implementation is
+//! done on a best-effort basis and differences in edge cases are highly likely.
+//!
+//! This crate implements the methods and deserialization logic to connect to a fgw endpoint during
+//! block sync. For an local implementation of the fgw, check out the fgw server under
+//! `gateway/server`.
+
 mod builder;
 mod methods;
 mod request_builder;
--- madara/crates/client/gateway/server/src/lib.rs
+++ madara/crates/client/gateway/server/src/lib.rs
@@ -1,3 +1,22 @@
+//! The **Feeder Gateway**, which you will sometimes see referred to as _fgw_, is an unspecified
+//! endpoint of a Starknet node. It is used for synchronization during block sync from a centralized
+//! source, predating Starknet's upgrade to p2p block sync.
+//!
+//! Despite this, the fgw has never been properly specified. Parity with mainnet implementation is
+//! done on a best-effort basis and differences in edge cases are highly likely.
+//!
+//! This crate implements a local fgw. For ways to connect, request and deserialize information from
+//! an existing fgw, check out the fgw client under `gateway/client`. Keep in mind that the fgw
+//! server is disabled by default and has to be explicitly enabled via the cli on node startup.
+//!
+//! # Available methods
+//!
+//! Below is a list of methods made available to the fgw. Keep in mind that the actual
+//! implementation is subject to subtle yet meaningful differences between nodes, and this includes
+//! the official Starknet fgw endpoints.
+//!
+//! TODO
+
 mod error;
 mod handler;
 mod helpers;
