3b67df396d5a224fa75934cc7352510422e8265d
Prakhar Pandey <prkpandey942@gmail.com>
2025-09-19 02:23:23 +0530

    update: inline max_gas_price_mul_factor for Ethereum settlement params

--- orchestrator/src/types/params/settlement.rs
+++ orchestrator/src/types/params/settlement.rs
@@ -27,7 +27,6 @@ impl TryFrom<RunCmd> for SettlementConfig {
                     Address::from_str(&run_cmd.ethereum_settlement_args.l1_core_contract_address.clone().ok_or(
                         OrchestratorError::SetupCommandError("L1 core contract address is required".to_string()),
                     )?)?;
-                let max_gas_price_mul_factor = run_cmd.ethereum_settlement_args.max_gas_price_mul_factor;
                 let ethereum_operator_address = Address::from_slice(
                     &hex::decode(
                         run_cmd
@@ -56,7 +55,7 @@ impl TryFrom<RunCmd> for SettlementConfig {
                     )?,
                     l1_core_contract_address,
                     starknet_operator_address: ethereum_operator_address,
-                    max_gas_price_mul_factor,
+                    max_gas_price_mul_factor: run_cmd.ethereum_settlement_args.max_gas_price_mul_factor,
                 };
                 Ok(Self::Ethereum(ethereum_params))
             }
