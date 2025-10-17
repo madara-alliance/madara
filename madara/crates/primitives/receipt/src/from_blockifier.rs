42f520b9d7e47f1bd9ed6b81a327d0d89d14f5ec
Heemank Verma <heemankv@gmail.com>
2025-10-17 11:18:09 +0530

    fix: cairo_native written more than once

--- madara/crates/primitives/receipt/src/from_blockifier.rs
+++ madara/crates/primitives/receipt/src/from_blockifier.rs
@@ -247,7 +247,6 @@ mod events_logic_tests {
             retdata: Default::default(),
             events,
             l2_to_l1_messages: vec![],
-            cairo_native: false,
             failed: false,
             gas_consumed: Default::default(),
             cairo_native: false,
