fe3ed5d55a6e90bf671df199c430e504957b06f8
Heemank Verma <heemankv@gmail.com>
2026-05-05 16:06:40 +0530

    fix(rpc): fail ws subscriptions after tx lag

--- madara/crates/client/rpc/src/versions/user/v0_10_2/methods/ws/subscribe_new_transactions.rs
+++ madara/crates/client/rpc/src/versions/user/v0_10_2/methods/ws/subscribe_new_transactions.rs
@@ -83,11 +83,11 @@ async fn subscribe_new_transactions_inner(
             received = async {
                 match &mut received_watch {
                     Some(watch) => watch.recv().await,
-                    None => pending::<Option<Arc<ValidatedTransaction>>>().await,
+                    None => pending::<Result<Option<Arc<ValidatedTransaction>>, crate::NewTransactionsWatchError>>().await,
                 }
             } => {
                 match received {
-                    Some(tx) => {
+                    Ok(Some(tx)) => {
                         send_validated_transaction(
                             &sink,
                             tx.as_ref(),
@@ -98,8 +98,11 @@ async fn subscribe_new_transactions_inner(
                             &mut emitted,
                         ).await?;
                     }
-                    None => {
+                    Ok(None) => {
                         received_watch = None;
+                    }
+                    Err(crate::NewTransactionsWatchError::Lagged) => {
+                        return Err(super::missed_received_transaction_notifications_error());
                     }
                 }
             }
