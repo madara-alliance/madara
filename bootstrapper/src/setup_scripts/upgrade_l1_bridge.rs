bab2e3f971f1facae44dfe39d157c0b3d030d650
adrienlacombe <6303520+adrienlacombe@users.noreply.github.com>
2026-06-08 12:10:12 +0200

    Fix bootstrapper artifact ABI paths

--- bootstrapper/src/setup_scripts/upgrade_l1_bridge.rs
+++ bootstrapper/src/setup_scripts/upgrade_l1_bridge.rs
@@ -17,8 +17,8 @@ abigen!(
     ]",
 );
 
-abigen!(EthereumNewBridge, "../build-artifacts/starkgate_latest/eth_bridge_upgraded.json");
-abigen!(EthereumNewBridgeEIC, "../build-artifacts/starkgate_latest/eic_eth_bridge.json");
+abigen!(EthereumNewBridge, "../build-artifacts/starkgate_latest/solidity/ethBridge.json");
+abigen!(EthereumNewBridgeEIC, "../build-artifacts/starkgate_latest/solidity/ethBridgeEIC.json");
 
 /// Upgrades the L1 Ethereum bridge implementation with a new version, including deployment of new
 /// contracts and configuration of administrative roles.
